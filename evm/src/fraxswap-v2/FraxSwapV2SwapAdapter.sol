6e42710538e965d7549b95ae52309777a425dfdb
domenicodev <domenico.romeo3919@gmail.com>
2023-12-19 16:17:00 +0100

    feat: Implemented price and updated pair type

--- evm/src/fraxswap-v2/FraxSwapV2SwapAdapter.sol
+++ evm/src/fraxswap-v2/FraxSwapV2SwapAdapter.sol
@@ -7,10 +7,10 @@ import {IERC20, ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
 // FraxSwapV2 handles arbirary amounts, but we limit the amount to 10x just in case
 uint256 constant RESERVE_LIMIT_FACTOR = 10;
 
-/// @title Integral Swap Adapter
+/// @title Frax Swap Adapter
 /// @dev Frax contracts do not use interfaces this much
 /// therefore copying the whole code would fill the file with 1000+ lines,
-/// but IUniswapV2Pair and IUniswapV2FactoryV5 perfectly fit as interfaces, so we can use them directly.
+/// but IUniswapV2PairPartialV5 and IUniswapV2FactoryV5 perfectly fit as interfaces, so we can use them directly.
 contract FraxSwapV2SwapAdapter is ISwapAdapter {
     IUniswapV2FactoryV5 immutable factory;
 
@@ -18,13 +18,26 @@ contract FraxSwapV2SwapAdapter is ISwapAdapter {
         factory = IUniswapV2FactoryV5(factory_);
     }
 
+    /// @inheritdoc ISwapAdapter
     function price(
         bytes32 _poolId,
         IERC20 _sellToken,
         IERC20 _buyToken,
         uint256[] memory _specifiedAmounts
     ) external view override returns (Fraction[] memory _prices) {
-        revert NotImplemented("FraxSwapV2SwapAdapter.price");
+        _prices = new Fraction[](_specifiedAmounts.length);
+        IUniswapV2PairPartialV5 pair = IUniswapV2PairPartialV5(address(bytes20(_poolId)));
+        uint112 r0;
+        uint112 r1;
+        if (address(_sellToken) == pair.token0()) { // sell
+            (r0, r1,) = pair.getReserves();
+        } else { // buy
+            (r1, r0,) = pair.getReserves();
+        }
+
+        for (uint256 i = 0; i < _specifiedAmounts.length; i++) {
+            _prices[i] = getPriceAt(_specifiedAmounts[i], r0, r1, pair);
+        }
     }
 
     function swap(
@@ -44,7 +57,7 @@ contract FraxSwapV2SwapAdapter is ISwapAdapter {
         override
         returns (uint256[] memory limits)
     {
-        IUniswapV2Pair pair = IUniswapV2Pair(address(bytes20(poolId)));
+        IUniswapV2PairPartialV5 pair = IUniswapV2PairPartialV5(address(bytes20(poolId)));
         limits = new uint256[](2);
         (uint256 r0, uint256 r1,) = pair.getReserves();
         if (address(sellToken) == pair.token0()) {
@@ -71,7 +84,7 @@ contract FraxSwapV2SwapAdapter is ISwapAdapter {
         returns (IERC20[] memory tokens)
     {
         tokens = new IERC20[](2);
-        IUniswapV2Pair pair = IUniswapV2Pair(address(bytes20(poolId)));
+        IUniswapV2PairPartialV5 pair = IUniswapV2PairPartialV5(address(bytes20(poolId)));
         tokens[0] = IERC20(pair.token0());
         tokens[1] = IERC20(pair.token1());
     }
@@ -91,6 +104,35 @@ contract FraxSwapV2SwapAdapter is ISwapAdapter {
         for (uint256 i = 0; i < ids.length; i++) {
             ids[i] = bytes20(factory.allPairs(offset + i));
         }
+    }
+
+    /// @notice Calculates pool prices after trade for specified amounts
+    /// @param amountIn The amount of the token being sold.
+    /// @param reserveIn The reserve of the token being sold.
+    /// @param reserveOut The reserve of the token being bought.
+    /// @param pair (IUniswapV2PairPartialV5) The pair where to execute the swap in.
+    /// @dev Basis points(BP) is 10,000 for Frax
+    function getPriceAt(uint256 amountIn, uint256 reserveIn, uint256 reserveOut, IUniswapV2PairPartialV5 pair)
+        internal
+        view
+        returns (Fraction memory)
+    {
+        if (reserveIn == 0 || reserveOut == 0) {
+            revert Unavailable("At least one reserve is zero!");
+        }
+        uint256 feeBP = pair.fee();
+
+        uint256 amountInWithFee = amountIn * feeBP;
+        uint256 numerator = amountInWithFee * reserveOut;
+        uint256 denominator = (reserveIn * 10000) + amountInWithFee;
+        uint256 amountOut = numerator / denominator;
+        uint256 newReserveOut = reserveOut - amountOut;
+        uint256 newReserveIn = reserveIn + amountIn;
+
+        return Fraction(
+            newReserveOut * 10000, 
+            newReserveIn * feeBP
+        );
     }
 }
 
@@ -113,83 +155,63 @@ interface IUniswapV2FactoryV5 {
     function toggleGlobalPause() external;
 }
 
-interface IUniswapV2Pair {
-    event Approval(
-        address indexed owner, address indexed spender, uint256 value
-    );
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
-    function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address owner) external view returns (uint256);
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 value) external returns (bool);
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value)
-        external
-        returns (bool);
-
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint256);
-
-    function permit(
-        address owner,
-        address spender,
-        uint256 value,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
-
-    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
-    event Burn(
-        address indexed sender,
-        uint256 amount0,
-        uint256 amount1,
-        address indexed to
-    );
+interface IUniswapV2PairPartialV5 {
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
     event Swap(
         address indexed sender,
-        uint256 amount0In,
-        uint256 amount1In,
-        uint256 amount0Out,
-        uint256 amount1Out,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
         address indexed to
     );
     event Sync(uint112 reserve0, uint112 reserve1);
 
-    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+    function MINIMUM_LIQUIDITY() external pure returns (uint);
     function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
-    function getReserves()
-        external
-        view
-        returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint256);
-    function price1CumulativeLast() external view returns (uint256);
-    function kLast() external view returns (uint256);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint);
+    function price1CumulativeLast() external view returns (uint);
+    function kLast() external view returns (uint);
+    function fee() external view returns (uint);
 
-    function mint(address to) external returns (uint256 liquidity);
-    function burn(address to)
-        external
-        returns (uint256 amount0, uint256 amount1);
-    function swap(
-        uint256 amount0Out,
-        uint256 amount1Out,
-        address to,
-        bytes calldata data
-    ) external;
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
     function skim(address to) external;
     function sync() external;
+    function initialize(address, address, uint) external;
 
-    function initialize(address, address) external;
+    // TWAMM
+
+    function longTermSwapFrom0To1(uint256 amount0In, uint256 numberOfTimeIntervals) external returns (uint256 orderId);
+    function longTermSwapFrom1To0(uint256 amount1In, uint256 numberOfTimeIntervals) external returns (uint256 orderId);
+    function cancelLongTermSwap(uint256 orderId) external;
+    function withdrawProceedsFromLongTermSwap(uint256 orderId) external returns (bool is_expired, address rewardTkn, uint256 totalReward);
+    function executeVirtualOrders(uint256 blockTimestamp) external;
+
+    function getAmountOut(uint amountIn, address tokenIn) external view returns (uint);
+    function getAmountIn(uint amountOut, address tokenOut) external view returns (uint);
+
+    function orderTimeInterval() external returns (uint256);
+    function getTWAPHistoryLength() external view returns (uint);
+    function getTwammReserves() external view returns (uint112 _reserve0, uint112 _reserve1, uint32 _blockTimestampLast, uint112 _twammReserve0, uint112 _twammReserve1, uint256 _fee);
+    function getReserveAfterTwamm(uint256 blockTimestamp) external view returns (uint112 _reserve0, uint112 _reserve1, uint256 lastVirtualOrderTimestamp, uint112 _twammReserve0, uint112 _twammReserve1);
+    function getNextOrderID() external view returns (uint256);
+    function getOrderIDsForUser(address user) external view returns (uint256[] memory);
+    function getOrderIDsForUserLength(address user) external view returns (uint256);
+    //    function getDetailedOrdersForUser(address user, uint256 offset, uint256 limit) external view returns (LongTermOrdersLib.Order[] memory detailed_orders);
+    function twammUpToDate() external view returns (bool);
+    function getTwammState() external view returns (uint256 token0Rate, uint256 token1Rate, uint256 lastVirtualOrderTimestamp, uint256 orderTimeInterval_rtn, uint256 rewardFactorPool0, uint256 rewardFactorPool1);
+    function getTwammSalesRateEnding(uint256 _blockTimestamp) external view returns (uint256 orderPool0SalesRateEnding, uint256 orderPool1SalesRateEnding);
+    function getTwammRewardFactor(uint256 _blockTimestamp) external view returns (uint256 rewardFactorPool0AtTimestamp, uint256 rewardFactorPool1AtTimestamp);
+    function getTwammOrder(uint256 orderId) external view returns (uint256 id, uint256 creationTimestamp, uint256 expirationTimestamp, uint256 saleRate, address owner, address sellTokenAddr, address buyTokenAddr);
+    function getTwammOrderProceedsView(uint256 orderId, uint256 blockTimestamp) external view returns (bool orderExpired, uint256 totalReward);
+    function getTwammOrderProceeds(uint256 orderId) external returns (bool orderExpired, uint256 totalReward);
+
+
+    function togglePauseNewSwaps() external;
 }
