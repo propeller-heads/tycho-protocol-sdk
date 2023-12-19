b49aded971b475e681e5ac09d17a3923fcd63aa0
domenicodev <domenico.romeo3919@gmail.com>
2023-12-19 11:38:13 +0100

    feat: Implemented getTokens

--- evm/src/fraxswap-v2/FraxSwapV2SwapAdapter.sol
+++ evm/src/fraxswap-v2/FraxSwapV2SwapAdapter.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.13;
 import {IERC20, ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
 
 /// @title Integral Swap Adapter
+/// @dev Frax contracts do not use interfaces this much
+/// therefore copying the whole code would fill the file with 1000+ lines,
+/// but IUniswapV2Pair fits as interface for Pairs, so we use it as interface.
 contract FraxSwapV2SwapAdapter is ISwapAdapter {
     IUniswapV2FactoryV5 immutable factory;
 
@@ -45,11 +48,17 @@ contract FraxSwapV2SwapAdapter is ISwapAdapter {
         revert NotImplemented("FraxSwapV2SwapAdapter.getCapabilities");
     }
 
+    /// @inheritdoc ISwapAdapter
     function getTokens(bytes32 poolId)
         external
+        view
+        override
         returns (IERC20[] memory tokens)
     {
-        revert NotImplemented("FraxSwapV2SwapAdapter.getTokens");
+        tokens = new IERC20[](2);
+        IUniswapV2Pair pair = IUniswapV2Pair(address(bytes20(poolId)));
+        tokens[0] = IERC20(pair.token0());
+        tokens[1] = IERC20(pair.token1());
     }
 
     /// @inheritdoc ISwapAdapter
@@ -87,4 +96,85 @@ interface IUniswapV2FactoryV5 {
     function setFeeTo(address) external;
     function setFeeToSetter(address) external;
     function toggleGlobalPause() external;
+}
+
+interface IUniswapV2Pair {
+    event Approval(
+        address indexed owner, address indexed spender, uint256 value
+    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address owner) external view returns (uint256);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value)
+        external
+        returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint256);
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(
+        address indexed sender,
+        uint256 amount0,
+        uint256 amount1,
+        address indexed to
+    );
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+    function factory() external view returns (address);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    function getReserves()
+        external
+        view
+        returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+    function price0CumulativeLast() external view returns (uint256);
+    function price1CumulativeLast() external view returns (uint256);
+    function kLast() external view returns (uint256);
+
+    function mint(address to) external returns (uint256 liquidity);
+    function burn(address to)
+        external
+        returns (uint256 amount0, uint256 amount1);
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+    function skim(address to) external;
+    function sync() external;
+
+    function initialize(address, address) external;
 }
