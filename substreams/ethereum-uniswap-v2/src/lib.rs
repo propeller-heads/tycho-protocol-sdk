465e163957ff5b97878a7ab3495c7c8902639e8a
Markus <m@haikane.com>
2024-03-21 14:48:38 +0700

    🤷‍♂️

--- substreams/ethereum-uniswap-v2/src/lib.rs
+++ substreams/ethereum-uniswap-v2/src/lib.rs
@@ -1,21 +1,14 @@
-use substreams::handlers::map;
-use substreams_ethereum::pb::eth::v2::Block;
-use tycho_substreams::prelude::*;
-use substreams::prelude::*;
-
-mod pb;
-mod pair_factory;
+mod abi;
 mod modules;
 
-#[map]
-pub fn map_changes(block: Block) -> Result<BlockContractChanges, substreams::errors::Error> {
-    modules::map_changes(block)
-}
-
-#[substreams::handlers::store]
-pub fn store_pools(
-    output: tycho::BlockContractChanges,
-    pool_store: substreams::store::StoreSetProto<ProtocolComponent>,
-) {
-    modules::store_pools(output, pool_store)
-}
+#[substreams::handlers::map]
+fn map_changes(
+    block: eth::v2::Block,
+) -> Result<tycho::BlockContractChanges, substreams::errors::Error> {
+    modules::map_protocol_changes(
+        block,
+        modules::map_components(block.clone()?)?,
+        modules::map_relative_balances(block.clone()?, modules::store_components_get())?,
+        modules::store_components_get(),
+        modules::store_balances_deltas(),
+    )
--- substreams/ethereum-uniswap-v2/src/modules.rs
+++ substreams/ethereum-uniswap-v2/src/modules.rs
@@ -1,257 +1,160 @@
-use crate::pair_factory;
-use std::collections::{hash_map::Entry, HashMap};
-use substreams::store::{StoreGet, StoreGetProto, StoreSetProto, StoreSet};
-use substreams_ethereum::pb::eth::v2::Block;
-use tycho_substreams::prelude::*;
-use hex_literal::hex;
+use crate::abi;
+use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashMap;
+use substreams::{hex, pb::substreams::StoreDeltas, store::{StoreAdd, StoreAddBigInt, StoreAddInt64, StoreGet, StoreGetInt64, StoreNew}};
+use substreams_ethereum::{pb::eth, Event};
+use tycho_substreams::{balances::aggregate_balances_changes, contract::extract_contract_changes, prelude::*};
 
-const UNISWAP_V2_FACTORY: [u8; 20] = hex!("5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+const FACTORY_ADDRESS: [u8; 20] = hex!("5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f");
+const PAIR_CREATED_EVENT_SIG: [u8; 32] = hex!("0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9");
+ 
+#[substreams::handlers::map]
+pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolComponents> {
+    Ok(BlockTransactionProtocolComponents {
+        tx_components: block
+            .events()
+            .filter(|event| event.address == FACTORY_ADDRESS && event.topics.first().unwrap() == &PAIR_CREATED_EVENT_SIG)
+            .flat_map(|event| {
+                let pair_address = event.params.get(2).unwrap().into_address().unwrap();
+                let component = ProtocolComponent {
+                    id: hex::encode(&pair_address),
+                    tokens: vec![
+                        event.params.get(0).unwrap().into_address().unwrap(),
+                        event.params.get(1).unwrap().into_address().unwrap(),
+                    ],
+                    contracts: vec![pair_address],
+                    change: tycho::ChangeType::Creation.into(),
+                };
 
-struct SlotValue {
-    new_value: Vec<u8>,
-    start_value: Vec<u8>,
-}
-
-impl SlotValue {
-    fn has_changed(&self) -> bool {
-        self.start_value != self.new_value
-    }
-}
-
-struct InterimContractChange {
-    address: Vec<u8>,
-    balance: Vec<u8>,
-    code: Vec<u8>,
-    slots: HashMap<Vec<u8>, SlotValue>,
-    change: ChangeType,
-}
-
-impl From<InterimContractChange> for ContractChange {
-    fn from(value: InterimContractChange) -> Self {
-        ContractChange {
-            address: value.address,
-            balance: value.balance,
-            code: value.code,
-            slots: value
-                .slots
-                .into_iter()
-                .filter(|(_, value)| value.has_changed())
-                .map(|(slot, value)| ContractSlot { slot, value: value.new_value })
-                .collect(),
-            change: value.change.into(),
-        }
-    }
-}
-
-pub fn map_changes(
-    block: Block,
-) -> Result<BlockContractChanges, substreams::errors::Error> {
-    let mut block_changes = BlockContractChanges::default();
-    let mut tx_change = TransactionContractChanges::default();
-    let mut changed_contracts: HashMap<Vec<u8>, InterimContractChange> = HashMap::new();
-
-    let created_accounts: HashMap<_, _> = block
-        .transactions()
-        .flat_map(|tx| {
-            tx.calls.iter().flat_map(|call| {
-                call.account_creations
-                    .iter()
-                    .map(|ac| (&ac.account, ac.ordinal))
+                Some(TransactionProtocolComponents { 
+                    tx: Some(event.receipt.transaction.into()),
+                    components: vec![component]
+                })
             })
+            .collect::<Vec<_>>(),
+    })
+}
+
+#[substreams::handlers::store]
+pub fn store_components(map: BlockTransactionProtocolComponents, store: StoreAddInt64) {
+    store.add_many(
+        0,
+        &map.tx_components
+            .iter()
+            .flat_map(|tx_components| &tx_components.components)
+            .map(|component| format!("pair:{0}", component.id))
+            .collect::<Vec<_>>(),
+        1,
+    );
+}
+
+#[substreams::handlers::get]
+pub fn store_components_get() -> StoreGetInt64 {
+    StoreGetInt64::new()
+}
+
+#[substreams::handlers::map]
+pub fn map_relative_balances(
+    block: eth::v2::Block,
+    store: StoreGetInt64
+) -> Result<BlockBalanceDeltas, anyhow::Error> {
+    let balance_deltas = block
+        .events()
+        .filter(|event| event.address == FACTORY_ADDRESS && event.topics.first().unwrap() == &hex!("ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"))
+        .flat_map(|event| {
+            let pair_address = event.address;
+            let component_id = pair_address.to_vec();
+
+            if store.get_last(format!("pair:{}", hex::encode(&component_id))).is_some() {
+                let token = event.params.get(2).unwrap().into_address().unwrap();
+                let amount = event.params.get(3).unwrap().into_bigint().unwrap();
+
+                vec![
+                    BalanceDelta {
+                        ord: event.ordinal(),
+                        tx: Some(event.receipt.transaction.into()),
+                        token: token.to_vec(),
+                        delta: amount.to_signed_bytes_be(),
+                        component_id,
+                    }
+                ]
+            } else {
+                vec![]
+            }
         })
-        .collect();
+        .collect::<Vec<_>>();
 
-    for block_tx in block.transactions() {
-        for call in block_tx.calls.iter().filter(|call| !call.state_reverted) {
-            if call.address == UNISWAP_V2_FACTORY
-                && call.input.len() >= 4
-                && call.input[0..4] == pair_factory::CREATE_PAIR_SIGNATURE
-            {
-                if let Ok(new_component) =
-                    pair_factory::decode_create_pair_call(call, &block_tx.into())
-                {
-                    tx_change.component_changes.push(new_component.clone());
-                }
-            }
-        }
-
-        let mut storage_changes = block_tx
-            .calls
-            .iter()
-            .filter(|call| !call.state_reverted)
-            .flat_map(|call| {
-                call.storage_changes.iter().filter(|c| {
-                    tx_change
-                        .component_changes
-                        .iter()
-                        .any(|component| component.contracts.contains(&call.address))
-                })
-            })
-            .collect::<Vec<_>>();
-        storage_changes.sort_unstable_by_key(|change| change.ordinal);
-
-        for storage_change in storage_changes.iter() {
-            match changed_contracts.entry(storage_change.address.clone()) {
-                Entry::Occupied(mut e) => {
-                    let contract_change = e.get_mut();
-                    match contract_change.slots.entry(storage_change.key.clone()) {
-                        Entry::Occupied(mut v) => {
-                            let slot_value = v.get_mut();
-                            slot_value.new_value.copy_from_slice(&storage_change.new_value);
-                        }
-                        Entry::Vacant(v) => {
-                            v.insert(SlotValue {
-                                new_value: storage_change.new_value.clone(),
-                                start_value: storage_change.old_value.clone(),
-                            });
-                        }
-                    }
-                }
-                Entry::Vacant(e) => {
-                    let mut slots = HashMap::new();
-                    slots.insert(
-                        storage_change.key.clone(),
-                        SlotValue {
-                            new_value: storage_change.new_value.clone(),
-                            start_value: storage_change.old_value.clone(),
-                        },
-                    );
-                    e.insert(InterimContractChange {
-                        address: storage_change.address.clone(),
-                        balance: Vec::new(),
-                        code: Vec::new(),
-                        slots,
-                        change: if created_accounts.contains_key(&storage_change.address) {
-                            ChangeType::Creation
-                        } else {
-                            ChangeType::Update
-                        },
-                    });
-                }
-            }
-        }
-
-        let mut balance_changes = block_tx
-            .calls
-            .iter()
-            .filter(|call| !call.state_reverted)
-            .flat_map(|call| {
-                call.balance_changes.iter().filter(|c| {
-                    tx_change
-                        .component_changes
-                        .iter()
-                        .any(|component| component.contracts.contains(&call.address))
-                })
-            })
-            .collect::<Vec<_>>();
-        balance_changes.sort_unstable_by_key(|change| change.ordinal);
-
-        for balance_change in balance_changes.iter() {
-            match changed_contracts.entry(balance_change.address.clone()) {
-                Entry::Occupied(mut e) => {
-                    let contract_change = e.get_mut();
-                    if let Some(new_balance) = &balance_change.new_value {
-                        contract_change.balance.clear();
-                        contract_change.balance.extend_from_slice(&new_balance.bytes);
-                    }
-                }
-                Entry::Vacant(e) => {
-                    if let Some(new_balance) = &balance_change.new_value {
-                        e.insert(InterimContractChange {
-                            address: balance_change.address.clone(),
-                            balance: new_balance.bytes.clone(),
-                            code: Vec::new(),
-                            slots: HashMap::new(),
-                            change: if created_accounts.contains_key(&balance_change.address) {
-                                ChangeType::Creation
-                            } else {
-                                ChangeType::Update
-                            },
-                        });
-                    }
-                }
-            }
-        }
-
-        let mut code_changes = block_tx
-            .calls
-            .iter()
-            .filter(|call| !call.state_reverted)
-            .flat_map(|call| {
-                call.code_changes.iter().filter(|c| {
-                    tx_change
-                        .component_changes
-                        .iter()
-                        .any(|component| component.contracts.contains(&call.address))
-                })
-            })
-            .collect::<Vec<_>>();
-        code_changes.sort_unstable_by_key(|change| change.ordinal);
-
-        for code_change in code_changes.iter() {
-            match changed_contracts.entry(code_change.address.clone()) {
-                Entry::Occupied(mut e) => {
-                    let contract_change = e.get_mut();
-                    contract_change.code.clear();
-                    contract_change.code.extend_from_slice(&code_change.new_code);
-                }
-                Entry::Vacant(e) => {
-                    e.insert(InterimContractChange {
-                        address: code_change.address.clone(),
-                        balance: Vec::new(),
-                        code: code_change.new_code.clone(),
-                        slots: HashMap::new(),
-                        change: if created_accounts.contains_key(&code_change.address) {
-                            ChangeType::Creation
-                        } else {
-                            ChangeType::Update
-                        },
-                    });
-                }
-            }
-        }
-
-        if !storage_changes.is_empty() || !balance_changes.is_empty() || !code_changes.is_empty() {
-            tx_change.tx = Some(Transaction {
-                hash: block_tx.hash.clone(),
-                from: block_tx.from.clone(),
-                to: block_tx.to.clone(),
-                index: block_tx.index as u64,
-            });
-
-            for (_, change) in changed_contracts.drain() {
-                tx_change.contract_changes.push(change.into());
-            }
-
-            block_changes.changes.push(tx_change.clone());
-
-            tx_change.tx = None;
-            tx_change.contract_changes.clear();
-        }
-    }
-
-    block_changes.block = Some(Block {
-        number: block.number,
-        hash: block.hash.clone(),
-        parent_hash: block
-            .header
-            .as_ref()
-            .expect("Block header not present")
-            .parent_hash
-            .clone(),
-        ts: block.header.as_ref().expect("Block header not present").timestamp,
-    }.into());
-
-    Ok(block_changes)
+    Ok(BlockBalanceDeltas { balance_deltas })
 }
 
-pub fn store_pools(
-    output: tycho::BlockContractChanges,
-    pool_store: substreams::store::StoreSetProto<ProtocolComponent>,
-) {
-    for tx_change in output.changes {
-        for component in tx_change.component_changes {
-            pool_store.set(format!("pool:{}", hex::encode(&component.contracts[0])), &component);
-        }
-    }
+#[substreams::handlers::store]
+pub fn store_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
+    tycho_substreams::balances::store_balance_changes(deltas, store);
+}
+
+#[substreams::handlers::store]
+pub fn store_balances_deltas() -> StoreDeltas {
+    StoreDeltas::new(StoreNew::<BigInt>::new())
+}
+
+#[substreams::handlers::map]
+pub fn map_protocol_changes(
+    block: eth::v2::Block,
+    grouped_components: BlockTransactionProtocolComponents,
+    deltas: BlockBalanceDeltas,
+    components_store: StoreGetInt64,
+    balance_store: StoreDeltas,
+) -> Result<BlockContractChanges> {
+    let mut transaction_contract_changes: HashMap<_, TransactionContractChanges> = HashMap::new();
+
+    grouped_components
+        .tx_components
+        .iter()
+        .for_each(|tx_component| {
+            let tx = tx_component.tx.as_ref().unwrap();
+            transaction_contract_changes
+                .entry(tx.index)
+                .or_insert_with(|| TransactionContractChanges::new(tx))
+                .component_changes
+                .extend_from_slice(&tx_component.components);
+        });
+
+    aggregate_balances_changes(balance_store, deltas)
+        .into_iter()
+        .for_each(|(_, (tx, balances))| {
+            transaction_contract_changes
+                .entry(tx.index)
+                .or_insert_with(|| TransactionContractChanges::new(&tx))
+                .balance_changes
+                .extend(balances.into_values());
+        });
+
+    extract_contract_changes(
+        &block,
+        |addr| {
+            components_store
+                .get_last(format!("pair:{0}", hex::encode(addr)))
+                .is_some()
+        },
+        &mut transaction_contract_changes,
+    );
+
+    Ok(BlockContractChanges {
+        block: Some((&block).into()),
+        changes: transaction_contract_changes
+            .drain()
+            .sorted_unstable_by_key(|(index, _)| *index)
+            .filter_map(|(_, change)| {
+                if change.contract_changes.is_empty() &&
+                    change.component_changes.is_empty() &&
+                    change.balance_changes.is_empty()
+                {
+                    None
+                } else {
+                    Some(change)
+                }
+            })
+            .collect::<Vec<_>>(),
+    })
 }
