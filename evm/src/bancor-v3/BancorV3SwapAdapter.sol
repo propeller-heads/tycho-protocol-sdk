fcd943c93758254383508b0808f669e509fbb0aa
mp-web3 <mp.web3.t@gmail.com>
2024-03-22 18:29:39 +0100

    feat in progress: working on getPriceAt

--- evm/src/bancor-v3/BancorV3SwapAdapter.sol
+++ evm/src/bancor-v3/BancorV3SwapAdapter.sol
@@ -4,9 +4,77 @@ pragma solidity ^0.8.13;
 import {IERC20, ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
 import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
+
+// TODO finish importing MathEx Library
+
+// https://etherscan.io/address/0xde1B3CcfC45e3F5bff7f43516F2Cd43364D883E4#code#F30#L194
+library MathEx {
+
+    /**
+     * @dev returns the largest integer smaller than or equal to `x * y / z`
+     */
+    function mulDivF(uint256 x, uint256 y, uint256 z) internal pure returns (uint256) {
+        Uint512 memory xy = mul512(x, y);
+
+        // if `x * y < 2 ^ 256`
+        if (xy.hi == 0) {
+            return xy.lo / z;
+        }
+
+        // assert `x * y / z < 2 ^ 256`
+        if (xy.hi >= z) {
+            revert Overflow();
+        }
+
+        uint256 m = _mulMod(x, y, z); // `m = x * y % z`
+        Uint512 memory n = _sub512(xy, m); // `n = x * y - m` hence `n / z = floor(x * y / z)`
+
+        // if `n < 2 ^ 256`
+        if (n.hi == 0) {
+            return n.lo / z;
+        }
+
+        uint256 p = _unsafeSub(0, z) & z; // `p` is the largest power of 2 which `z` is divisible by
+        uint256 q = _div512(n, p); // `n` is divisible by `p` because `n` is divisible by `z` and `z` is divisible by `p`
+        uint256 r = _inv256(z / p); // `z / p = 1 mod 2` hence `inverse(z / p) = 1 mod 2 ^ 256`
+        return _unsafeMul(q, r); // `q * r = (n / p) * inverse(z / p) = n / z`
+    }
+
+    /**
+     * @dev returns the value of `x * y`
+     */
+    function mul512(uint256 x, uint256 y) internal pure returns (Uint512 memory) {
+        uint256 p = _mulModMax(x, y);
+        uint256 q = _unsafeMul(x, y);
+        if (p >= q) {
+            return Uint512({ hi: p - q, lo: q });
+        }
+        
+        return Uint512({ hi: _unsafeSub(p, q) - 1, lo: q });
+    }
+
+    /**
+     * @dev returns `(x * y) % 2 ^ 256`
+     */
+    function _unsafeMul(uint256 x, uint256 y) private pure returns (uint256) {
+        unchecked {
+            return x * y;
+        }
+    }
+
+    /**
+     * @dev returns `x * y % (2 ^ 256 - 1)`
+     */
+    function _mulModMax(uint256 x, uint256 y) private pure returns (uint256) {
+        return mulmod(x, y, type(uint256).max);
+    }
+}
+
 /// @title BancorV3Swap Adapter
 contract BancorV3SwapAdapter is ISwapAdapter {
     using SafeERC20 for IERC20;
+    
+    uint32 constant PPM_RESOLUTION = 1_000_000;
 
     IBancorV3BancorNetwork public immutable bancorNetwork;
     IBancorV3BancorNetworkInfo public immutable bancorNetworkInfo;
@@ -35,25 +103,76 @@ contract BancorV3SwapAdapter is ISwapAdapter {
     }
 
     function price(
-        bytes32 _poolId,
+        bytes32,
         IERC20 _sellToken,
         IERC20 _buyToken,
         uint256[] memory _specifiedAmounts
     ) external view override returns (Fraction[] memory _prices) {
-        revert NotImplemented("TemplateSwapAdapter.price");
+        
     }
 
-    function getPriceAt(uint256 amountIn, IERC20 _sellToken, IERC20 _buyToken) 
+    // 4. Price Function
+    // Chiamare tradeOutputAndFeeBySourceAmount e usare output per aggiornare le riserve 
+    // Aggiorno le riserve con le formule che ho già trovato
+    // Chiamo la funzione per Fee PPM (PoolCollection)
+    // Riproduco in locale la formula di tradeOutputAndFeeBySourceAmount con input amount millesimale rispetto alla riserva
+    // Numeratore: uint256 targetAmount = MathEx.mulDivF(targetBalance, sourceAmount, sourceBalance + sourceAmount) - uint256 tradingFeeAmount = MathEx.mulDivF(targetAmount, feePPM, PPM_RESOLUTION);
+    // Denominatore: Input amount (millesimale)
+
+    function getPriceAt(uint256 _amountIn, IERC20 _sellToken, IERC20 _buyToken) 
     external
     view
     onlySupportedTokens(address(_sellToken), address(_buyToken))
     returns (Fraction memory)
     {   
+        uint256 numerator;
+        uint256 denominator;
+        
         Token sellToken = Token(address(_sellToken));
         Token buyToken = Token(address(_buyToken));
-        uint256 numerator = bancorNetworkInfo.tradeOutputBySourceAmount(sellToken, buyToken, 1);
+        Token BNT = Token(address(bnt));
 
-        return Fraction(0,0);
+        uint256 amountIn = _amountIn;
+
+        if (sellToken == BNT || buyToken == BNT) {
+        
+        (uint256 tradingLiquiditySellTokenBefore, uint256 tradingLiquidityBuyTokenBefore) = getTradingLiquidityBntPool(sellToken, buyToken);
+
+        bancorPoolCollection.TradeAmountAndFee memory tf = bancorPoolCollection.tradeOutputAndFeeBySourceAmount(sellToken, buyToken, sourceAmount);
+
+            if (sellToken == BNT) {
+
+                uint256 calculatedLiquiditySellTokenAfter = tradingLiquiditySellTokenBefore + (amountIn - tf.networkFeeAmount);
+                uint256 calculatedLiquidityBuyTokenAfter = tradingLiquidityBuyTokenBefore - tf.amount;
+
+                uint32 feePPM = bancorNetworkInfo.tradingFeePPM(buyToken);
+
+            } else {
+
+                calculatedLiquiditySellTokenAfter = tradingLiquiditySellTokenBefore + amountIn;
+                calculatedLiquidityBuyTokenAfter = tradingLiquidityBuyTokenBefore - (tf.amount + tf.networkFeeAmount);
+                
+                feePPM = bancorNetworkInfo.tradingFeePPM(sellToken);
+
+            }
+
+            uint256 punctualAmountIn = calculatedLiquiditySellTokenAfter/1000;
+            uint256 targetAmount = MathEx.mulDivF(calculatedLiquidityBuyTokenAfter, punctualAmountIn, calculatedLiquiditySellTokenAfter + punctualAmountIn);
+            uint256 tradingFeeAmount = MathEx.mulDivF(targetAmount, feePPM, PPM_RESOLUTION); /// What is PPM_RESOLUTION?
+            
+            uint256 missingDecimalsSellToken = 18 - uint256(IERC20Detailed(address(_sellToken)).decimals());
+            uint256 missingDecimalsBuyToken = 18 - uint256(IERC20Detailed(address(_buyToken)).decimals());
+
+            numerator = (targetAmount - tradingFeeAmount) * 10 ** missingDecimalsBuyToken;
+            denominator = punctualAmountIn * 10 ** missingDecimalsSellToken;
+
+            return Fraction(numerator, denominator);
+
+        } else {
+
+            return Fraction(0,0);
+        }
+
     }
 
     /// @inheritdoc ISwapAdapter
@@ -247,6 +366,22 @@ contract BancorV3SwapAdapter is ISwapAdapter {
         ids = new bytes32[](endIdx - offset);
         for (uint256 i = 0; i < ids.length; i++) {
             ids[i] = bytes20(address(tokenPools[offset + i]));
+        }
+    }
+
+    function getTradingLiquidityBntPool(Token _sellToken, Token _buyToken) 
+    internal 
+    returns (uint256 tradingLiquiditySellToken, uint256 tradingLiquidityBuyToken) 
+    {
+        TradingLiquidity memory tradingLiquidityPool;
+        if (sellToken == bnt) {
+            tradingLiquidityPool = getTradingLiquidity(_buyToken);
+            tradingLiquiditySellToken = uint256(tradingLiquidityPool.bntTradingLiquidity);
+            tradingLiquidityBuyToken = uint256(tradingLiquidityPool.baseTokenTradingLiquidity);
+        } else {
+            tradingLiquidityPool = getTradingLiquidity(_sellToken);
+            tradingLiquiditySellToken = uint256(tradingLiquidityPool.baseTokenTradingLiquidity);
+            tradingLiquidityBuyToken = uint256(tradingLiquidityPool.bntTradingLiquidity);
         }
     }
 
