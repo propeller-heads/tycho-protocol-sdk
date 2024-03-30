37c5806b79e284a7f62a842a94930124a77c775a
Ignazio Bovo <ignazio@jsgenesis.com>
2024-03-30 17:44:45 +0100

    style: :adhesive_bandage: formatting

--- substreams/ethereum-frax/build.rs
+++ substreams/ethereum-frax/build.rs
@@ -1,22 +1,15 @@
 use anyhow::{Ok, Result};
 use regex::Regex;
-use substreams_ethereum::Abigen;
 use std::fs;
+use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), anyhow::Error> {
-    let file_names = [
-        "abi/factory_contract.abi.json",
-        "abi/pair_contract.abi.json",
-    ];
-    let file_output_names = [
-        "src/abi/factory_contract.rs",
-        "src/abi/pair_contract.rs",
-    ];
+    let file_names = ["abi/factory_contract.abi.json", "abi/pair_contract.abi.json"];
+    let file_output_names = ["src/abi/factory_contract.rs", "src/abi/pair_contract.rs"];
 
     let mut i = 0;
     for f in file_names {
-        let contents = fs::read_to_string(f)
-            .expect("Should have been able to read the file");
+        let contents = fs::read_to_string(f).expect("Should have been able to read the file");
 
         // sanitize fields and attributes starting with an underscore
         let regex = Regex::new(r#"("\w+"\s?:\s?")_(\w+")"#).unwrap();
@@ -26,7 +19,7 @@ fn main() -> Result<(), anyhow::Error> {
             .generate()?
             .write_to_file(file_output_names[i])?;
 
-        i = i+1;
+        i += 1;
     }
 
     Ok(())
