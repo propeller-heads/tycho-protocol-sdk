15227e9f80ac754cce22a963593537e28c78461f
Ignazio Bovo <ignazio@jsgenesis.com>
2024-03-30 17:22:07 +0100

    fix: :green_heart: substreams build process errors

--- substreams/ethereum-template/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use substreams_ethereum::pb::eth;
-mod modules;
-
-#[substreams::handlers::map]
-fn map_changes(
-    block: eth::v2::Block,
-) -> Result<tycho::BlockContractChanges, substreams::errors::Error> {
-    todo!("Not implemented")
-}
