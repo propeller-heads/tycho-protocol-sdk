6dadd21def4a0bab6ee86976f4a9f28533059890
domenicodev <domenico.romeo3919@gmail.com>
2024-04-17 15:58:24 +0200

    Initial Setup

---
+++ evm/src/kyberswap-classic/KyberSwapClassicAdapter.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import {ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
+
+/// @title Kyberswap Classic Adapter
+contract KyberSwapClassicAdapter is ISwapAdapter {
+    function price(
+        bytes32 _poolId,
+        address _sellToken,
+        address _buyToken,
+        uint256[] memory _specifiedAmounts
+    ) external view override returns (Fraction[] memory _prices) {
+        revert NotImplemented("TemplateSwapAdapter.price");
+    }
+
+    function swap(
+        bytes32 poolId,
+        address sellToken,
+        address buyToken,
+        OrderSide side,
+        uint256 specifiedAmount
+    ) external returns (Trade memory trade) {
+        revert NotImplemented("TemplateSwapAdapter.swap");
+    }
+
+    function getLimits(bytes32 poolId, address sellToken, address buyToken)
+        external
+        returns (uint256[] memory limits)
+    {
+        revert NotImplemented("TemplateSwapAdapter.getLimits");
+    }
+
+    function getCapabilities(
+        bytes32 poolId,
+        address sellToken,
+        address buyToken
+    ) external returns (Capability[] memory capabilities) {
+        revert NotImplemented("TemplateSwapAdapter.getCapabilities");
+    }
+
+    function getTokens(bytes32 poolId)
+        external
+        returns (address[] memory tokens)
+    {
+        revert NotImplemented("TemplateSwapAdapter.getTokens");
+    }
+
+    function getPoolIds(uint256 offset, uint256 limit)
+        external
+        returns (bytes32[] memory ids)
+    {
+        revert NotImplemented("TemplateSwapAdapter.getPoolIds");
+    }
+}
