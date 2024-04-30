8213f9073dac1a16ec456728cecbd57de3026eb8
domenicodev <domenico.romeo3919@gmail.com>
2024-04-30 18:03:15 +0200

    feat: Finished tests and fixed functions

--- evm/test/KyberSwapElasticAdapter.t.sol
+++ evm/test/KyberSwapElasticAdapter.t.sol
@@ -11,66 +11,113 @@ contract KyberSwapElasticAdapterTest is Test, ISwapAdapterTypes {
     using FractionMath for Fraction;
 
     KyberSwapElasticAdapter adapter;
-    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address constant USDT = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address constant USDT_WETH_PAIR = 0xF138462C76568CDFD77C6EB831E973D6963F2006;
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant DAI_USDC_PAIR = 0xa847a0DDc1eAB013D2C8f6D8b37766DF99776BC6;
 
     uint256 constant TEST_ITERATIONS = 100;
 
     function setUp() public {
-        uint256 forkBlock = 18747990;
+        uint256 forkBlock = 18384222;
         vm.createSelectFork(vm.rpcUrl("mainnet"), forkBlock);
         adapter =
             new KyberSwapElasticAdapter(0xC7a590291e07B9fe9E64b86c58fD8fC764308C4A);
 
         vm.label(address(adapter), "KyberSwapElasticAdapter");
-        vm.label(WETH, "WETH");
-        vm.label(USDT, "USDT");
-        vm.label(USDT_WETH_PAIR, "USDT_WETH_PAIR");
+        vm.label(USDC, "USDC");
+        vm.label(DAI, "DAI");
+        vm.label(DAI_USDC_PAIR, "DAI_USDC_PAIR");
     }
 
     function testSwapFuzzKyberSwapElastic(uint256 specifiedAmount, bool isBuy) public {
         OrderSide side = isBuy ? OrderSide.Buy : OrderSide.Sell;
 
-        bytes32 pair = bytes32(bytes20(USDT_WETH_PAIR));
-        uint256[] memory limits = adapter.getLimits(pair, USDT, WETH);
+        bytes32 pair = bytes32(bytes20(DAI_USDC_PAIR));
+        uint256[] memory limits = adapter.getLimits(pair, DAI, USDC);
 
         if (side == OrderSide.Buy) {
-            vm.assume(specifiedAmount < limits[1]);
+            vm.assume(specifiedAmount < limits[1] && specifiedAmount > 1);
 
-            deal(USDT, address(this), type(uint256).max);
-            IERC20(USDT).approve(address(adapter), type(uint256).max);
+            deal(DAI, address(this), type(uint256).max);
+            IERC20(DAI).approve(address(adapter), type(uint256).max);
         } else {
-            vm.assume(specifiedAmount < limits[0]);
+            vm.assume(specifiedAmount < limits[0] && specifiedAmount > 1);
 
-            deal(USDT, address(this), specifiedAmount);
-            IERC20(USDT).approve(address(adapter), specifiedAmount);
+            deal(DAI, address(this), specifiedAmount);
+            IERC20(DAI).approve(address(adapter), specifiedAmount);
         }
 
-        uint256 USDT_balance = IERC20(USDT).balanceOf(address(this));
-        uint256 weth_balance = IERC20(WETH).balanceOf(address(this));
+        uint256 DAI_balance = IERC20(DAI).balanceOf(address(this));
+        uint256 USDC_balance = IERC20(USDC).balanceOf(address(this));
 
         Trade memory trade =
-            adapter.swap(pair, USDT, WETH, side, specifiedAmount);
+            adapter.swap(pair, DAI, USDC, side, specifiedAmount);
 
         if (trade.calculatedAmount > 0) {
             if (side == OrderSide.Buy) {
                 assertEq(
                     specifiedAmount,
-                    IERC20(WETH).balanceOf(address(this)) - weth_balance
+                    IERC20(USDC).balanceOf(address(this)) - USDC_balance
                 );
                 assertEq(
                     trade.calculatedAmount,
-                    USDT_balance - IERC20(USDT).balanceOf(address(this))
+                    DAI_balance - IERC20(DAI).balanceOf(address(this))
                 );
             } else {
                 assertEq(
                     specifiedAmount,
-                    USDT_balance - IERC20(USDT).balanceOf(address(this))
+                    DAI_balance - IERC20(DAI).balanceOf(address(this))
                 );
                 assertEq(
                     trade.calculatedAmount,
-                    IERC20(WETH).balanceOf(address(this)) - weth_balance
+                    IERC20(USDC).balanceOf(address(this)) - USDC_balance
+                );
+            }
+        }
+    }
+
+    function testSwapFuzzKyberSwapElasticInverse(uint256 specifiedAmount, bool isBuy) public {
+        OrderSide side = isBuy ? OrderSide.Buy : OrderSide.Sell;
+
+        bytes32 pair = bytes32(bytes20(DAI_USDC_PAIR));
+        uint256[] memory limits = adapter.getLimits(pair, USDC, DAI);
+
+        if (side == OrderSide.Buy) {
+            vm.assume(specifiedAmount < limits[1] && specifiedAmount > 1);
+
+            deal(USDC, address(this), type(uint256).max);
+            IERC20(USDC).approve(address(adapter), type(uint256).max);
+        } else {
+            vm.assume(specifiedAmount < limits[0] && specifiedAmount > 1);
+
+            deal(USDC, address(this), specifiedAmount);
+            IERC20(USDC).approve(address(adapter), specifiedAmount);
+        }
+
+        uint256 DAI_balance = IERC20(DAI).balanceOf(address(this));
+        uint256 USDC_balance = IERC20(USDC).balanceOf(address(this));
+
+        Trade memory trade =
+            adapter.swap(pair, USDC, DAI, side, specifiedAmount);
+
+        if (trade.calculatedAmount > 0) {
+            if (side == OrderSide.Buy) {
+                assertEq(
+                    specifiedAmount,
+                    IERC20(DAI).balanceOf(address(this)) - DAI_balance
+                );
+                assertEq(
+                    trade.calculatedAmount,
+                    USDC_balance - IERC20(USDC).balanceOf(address(this))
+                );
+            } else {
+                assertEq(
+                    specifiedAmount,
+                    USDC_balance - IERC20(USDC).balanceOf(address(this))
+                );
+                assertEq(
+                    trade.calculatedAmount,
+                    IERC20(DAI).balanceOf(address(this)) - DAI_balance
                 );
             }
         }
@@ -81,7 +128,7 @@ contract KyberSwapElasticAdapterTest is Test, ISwapAdapterTypes {
     }
 
     function executeIncreasingSwaps(OrderSide side) internal {
-        bytes32 pair = bytes32(bytes20(USDT_WETH_PAIR));
+        bytes32 pair = bytes32(bytes20(DAI_USDC_PAIR));
 
         uint256[] memory amounts = new uint256[](TEST_ITERATIONS);
         for (uint256 i = 0; i < TEST_ITERATIONS; i++) {
@@ -93,16 +140,15 @@ contract KyberSwapElasticAdapterTest is Test, ISwapAdapterTypes {
         for (uint256 i = 0; i < TEST_ITERATIONS; i++) {
             beforeSwap = vm.snapshot();
 
-            deal(USDT, address(this), amounts[i]);
-            IERC20(USDT).approve(address(adapter), amounts[i]);
+            deal(USDC, address(this), amounts[i]);
+            IERC20(USDC).approve(address(adapter), amounts[i]);
 
-            trades[i] = adapter.swap(pair, USDT, WETH, side, amounts[i]);
+            trades[i] = adapter.swap(pair, USDC, DAI, side, amounts[i]);
             vm.revertTo(beforeSwap);
         }
 
         for (uint256 i = 1; i < TEST_ITERATIONS - 1; i++) {
             assertLe(trades[i].calculatedAmount, trades[i + 1].calculatedAmount);
-            assertLe(trades[i].gasUsed, trades[i + 1].gasUsed);
         }
     }
 
@@ -117,9 +163,16 @@ contract KyberSwapElasticAdapterTest is Test, ISwapAdapterTypes {
     }
 
     function testGetLimitsKyberSwapElastic() public {
-        bytes32 pair = bytes32(bytes20(USDT_WETH_PAIR));
-        uint256[] memory limits = adapter.getLimits(pair, USDT, WETH);
+        bytes32 pair = bytes32(bytes20(DAI_USDC_PAIR));
+        uint256[] memory limits = adapter.getLimits(pair, DAI, USDC);
 
         assertEq(limits.length, 2);
+    }
+
+    function testGetTokensKyberSwapElastic() public {
+        bytes32 pair = bytes32(bytes20(DAI_USDC_PAIR));
+        address[] memory tokens = adapter.getTokens(pair);
+
+        assertEq(tokens.length, 2);
     }
 }
