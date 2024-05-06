b64b4fd0b9533abf4e6ec8cbfeb113257f7f8cb0
mp-web3 <mp.web3.t@gmail.com>
2024-05-06 10:49:48 +0200

    Initialized tests

--- evm/test/sDaiSwapAdapter.t.sol
+++ evm/test/sDaiSwapAdapter.t.sol
@@ -4,13 +4,29 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "src/interfaces/ISwapAdapterTypes.sol";
 import "src/libraries/FractionMath.sol";
+import "src/sDai/sDaiSwapAdapter.sol";
+import "forge-std/console.sol";
+import "forge-std/console2.sol";
 
 /// @title sDaiSwapAdapterTest
 
 contract sDaiSwapAdapterTest is Test, ISwapAdapterTypes {
     using FractionMath for Fraction;
 
-    function testPriceFuzz(uint256 amount0, uint256 amount1) public {}
+    sDaiSwapAdapter adapter;
+    ISavingsDai savingsDai;
 
-    function testSwapFuzz(uint256 specifiedAmount) public {}
+    address constant SAVINGS_DAI_ADDRESS = 0x83F20F44975D03b1b09e64809B757c47f942BEeA;
+
+    function setUp() public {
+        uint256 forkBlock = 18835309;
+        vm.createSelectFork(vm.rpcUrl("mainnet"), forkBlock);
+        adapter = new sDaiSwapAdapter(SAVINGS_DAI_ADDRESS);
+    }
+
+    function testGetAssetAddress() public {
+        address dai = adapter.getAssetAddress();
+        console.log("Dai address", dai);
+    }
+
 }
