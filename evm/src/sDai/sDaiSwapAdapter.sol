0bce026603d23f64a8a9f39b2bfe27870e58f4fb
mp-web3 <mp.web3.t@gmail.com>
2024-05-06 10:49:48 +0200

    Initialized tests

--- evm/src/sDai/sDaiSwapAdapter.sol
+++ evm/src/sDai/sDaiSwapAdapter.sol
@@ -2,10 +2,24 @@
 pragma solidity ^0.8.13;
 
 import {ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
+import {IERC20Metadata} from
+    "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {
+    IERC20,
+    SafeERC20
+} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title sDaiSwapAdapter
 
 contract sDaiSwapAdapter is ISwapAdapter {
+    using SafeERC20 for IERC20;
+
+    ISavingsDai immutable savingsDai; 
+
+    constructor(address savingsDai_) {
+        savingsDai = ISavingsDai(savingsDai_);
+    }
+
     function price(
         bytes32 _poolId,
         address _sellToken,
@@ -53,4 +67,21 @@ contract sDaiSwapAdapter is ISwapAdapter {
     {
         revert NotImplemented("TemplateSwapAdapter.getPoolIds");
     }
+
+
+    ///// TEST FUNCTIONS /////
+
+    function getAssetAddress() external view returns (address) {
+
+        return savingsDai.asset();
+    }
+}
+
+interface ISavingsDai {
+
+
+    function asset() external view returns (address);
+
+    function decimals() external view returns (uint8);
+
 }
