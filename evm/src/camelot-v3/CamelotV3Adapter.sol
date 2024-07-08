855b61ae322591ee6c258c78f8df9c3a82d09968
domenicodev <domenico.romeo3919@gmail.com>
2024-07-08 17:42:24 +0200

    feat: Implemented getLimits() and getTokens()

--- evm/src/camelot-v3/CamelotV3Adapter.sol
+++ evm/src/camelot-v3/CamelotV3Adapter.sol
@@ -2,12 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {ISwapAdapter} from "src/interfaces/ISwapAdapter.sol";
+import {
+    IERC20,
+    SafeERC20
+} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-/// @title TemplateSwapAdapter
-/// @dev This is a template for a swap adapter.
-/// Rename it to your own protocol's name and implement it according to the
-/// specification.
-contract TemplateSwapAdapter is ISwapAdapter {
+/// @title Camelot V3 Adapter
+contract CamelotV3Adapter is ISwapAdapter {
+    using SafeERC20 for IERC20;
+
+    IQuoter immutable quoter;
+    IAlgebraFactory immutable factory;
+
+    constructor (address _quoter) {
+        quoter = IQuoter(_quoter);
+        factory = IAlgebraFactory(quoter.factory());
+    }
+
     function price(
         bytes32 _poolId,
         address _sellToken,
@@ -27,11 +38,16 @@ contract TemplateSwapAdapter is ISwapAdapter {
         revert NotImplemented("TemplateSwapAdapter.swap");
     }
 
+    /// @inheritdoc ISwapAdapter
     function getLimits(bytes32 poolId, address sellToken, address buyToken)
         external
         returns (uint256[] memory limits)
     {
-        revert NotImplemented("TemplateSwapAdapter.getLimits");
+        address poolAddress = address(bytes20(poolId));
+        IAlgebraPool pool = IAlgebraPool(poolAddress);
+        limits = new uint256[](2);
+        limits[0] = IERC20(sellToken).balanceOf(poolAddress);
+        limits[1] = IERC20(buyToken).balanceOf(poolAddress);
     }
 
     function getCapabilities(
@@ -42,17 +58,68 @@ contract TemplateSwapAdapter is ISwapAdapter {
         revert NotImplemented("TemplateSwapAdapter.getCapabilities");
     }
 
+    /// @inheritdoc ISwapAdapter
     function getTokens(bytes32 poolId)
         external
+        view
         returns (address[] memory tokens)
     {
-        revert NotImplemented("TemplateSwapAdapter.getTokens");
+        IAlgebraPool pool = IAlgebraPool(address(bytes20(poolId)));
+        tokens = new address[](2);
+        tokens[0] = pool.token0();
+        tokens[1] = pool.token1();
     }
 
-    function getPoolIds(uint256 offset, uint256 limit)
+    function getPoolIds(uint256, uint256)
         external
-        returns (bytes32[] memory ids)
+        pure
+        returns (bytes32[] memory)
     {
-        revert NotImplemented("TemplateSwapAdapter.getPoolIds");
+        revert NotImplemented("CamelotV3Adapter.getPoolIds");
     }
+}
+
+interface IQuoter {
+    function quoteExactInput(bytes memory path, uint256 amountIn)
+        external
+        returns (uint256 amountOut, uint16[] memory fees);
+
+    function quoteExactInputSingle(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint160 limitSqrtPrice
+    ) external returns (uint256 amountOut, uint16 fee);
+
+    function quoteExactOutput(bytes memory path, uint256 amountOut)
+        external
+        returns (uint256 amountIn, uint16[] memory fees);
+
+    function quoteExactOutputSingle(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountOut,
+        uint160 limitSqrtPrice
+    ) external returns (uint256 amountIn, uint16 fee);
+
+    function factory() external view returns (address);
+}
+
+interface IAlgebraFactory {
+
+  function poolDeployer() external view returns (address);
+
+  function farmingAddress() external view returns (address);
+
+  function vaultAddress() external view returns (address);
+
+  function poolByPair(address tokenA, address tokenB) external view returns (address pool);
+}
+
+interface IAlgebraPool {
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function swap(address recipient, bool zero2one, int256 amountRequired, uint160 limitSqrtPrice, bytes calldata data) external returns (int256 amount0, int256 amount1);
 }
