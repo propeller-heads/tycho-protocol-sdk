3096b0fcd5d25f8c4de2f75a7f1cb914e4e8139d
Ignazio Bovo <ignazio@jsgenesis.com>
2024-09-16 09:12:20 +0200

    build: :truck: remove unnecessary pb/tycho

--- substreams/ethereum-sfraxeth/src/pb/mod.rs
+++ substreams/ethereum-sfraxeth/src/pb/mod.rs
@@ -6,12 +6,3 @@ pub mod contract {
         // @@protoc_insertion_point(contract.v1)
     }
 }
-pub mod tycho {
-    pub mod evm {
-        // @@protoc_insertion_point(attribute:tycho.evm.v1)
-        pub mod v1 {
-            include!("tycho.evm.v1.rs");
-            // @@protoc_insertion_point(tycho.evm.v1)
-        }
-    }
-}
