49875b728e03c4c3d159ddbfa8ce80b3b7d3f094
domenicodev <domenico.romeo3919@gmail.com>
2024-10-09 12:01:44 +0200

    feat: added LiquidityPool contract events

--- substreams/ethereum-etherfi/src/consts.rs
+++ substreams/ethereum-etherfi/src/consts.rs
@@ -1,9 +1,9 @@
 use substreams::hex;
 
 // Liquidity Pools
-pub const LIQUIDITY_POOL_ADDRESS: [u8; 20] = hex!("6A8cbed756804B16E05E741eDaBd5cB544AE21bf");
+pub const LIQUIDITY_POOL_ADDRESS: [u8; 20] = hex!("308861A430be4cce5502d0A12724771Fc6DaF216");
 
 // Tokens
-pub const EETH_ADDRESS: [u8; 20] = hex!("F18056Bbd320E96A48e3Fbf8bC061322531aac99");
-pub const WEETH_ADDRESS: [u8; 20] = hex!("B9fC157394Af804a3578134A6585C0dc9cc990d4");
-pub const ETH_ADDRESS: [u8; 20] = hex!("0959158b6040D32d04c301A72CBFD6b39E21c9AE");
+pub const EETH_ADDRESS: [u8; 20] = hex!("35fA164735182de50811E8e2E824cFb9B6118ac2");
+pub const WEETH_ADDRESS: [u8; 20] = hex!("Cd5fE23C85820F7B72D0926FC9b05b43E359b7ee");
+pub const ETH_ADDRESS: [u8; 20] = hex!("EeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
