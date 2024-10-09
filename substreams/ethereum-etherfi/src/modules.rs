4ff347572c59724ebeeb9e1171cc53d18cd85d17
domenicodev <domenico.romeo3919@gmail.com>
2024-10-09 12:01:44 +0200

    feat: added LiquidityPool contract events

--- substreams/ethereum-etherfi/src/modules.rs
+++ substreams/ethereum-etherfi/src/modules.rs
@@ -1,25 +1,25 @@
-use crate::abi;
+use crate::{abi, consts};
 use anyhow::Result;
+use consts::{EETH_ADDRESS, ETH_ADDRESS, LIQUIDITY_POOL_ADDRESS, WEETH_ADDRESS};
 use itertools::Itertools;
 use std::collections::HashMap;
-use consts::{EETH_ADDRESS, WEETH_ADDRESS, ETH_ADDRESS, LIQUIDITY_POOL_ADDRESS};
 use substreams::{
+    hex,
     pb::substreams::StoreDeltas,
     store::{
-        StoreAddBigInt, StoreGet, StoreGetInt64, StoreGetProto, StoreNew, StoreSet, StoreSetProto,
+        StoreAddBigInt, StoreGet, StoreGetString, StoreGetProto, StoreNew, StoreSet, StoreSetProto,
     },
 };
 use substreams_ethereum::{pb::eth, Event};
 use tycho_substreams::{
-    balances::aggregate_balances_changes, contract::extract_contract_changes, prelude::*,
+    balances::aggregate_balances_changes, contract::extract_contract_changes_builder, prelude::*,
 };
 
 #[substreams::handlers::map]
 pub fn map_components(
     params: String,
     block: eth::v2::Block,
-) -> Result<BlockTransactionProtocolComponents> {
-
+) -> Result<BlockTransactionProtocolComponents, anyhow::Error> {
     Ok(BlockTransactionProtocolComponents {
         tx_components: block
             .transactions()
@@ -27,19 +27,16 @@ pub fn map_components(
                 let components = tx
                     .logs_with_calls()
                     .filter(|(_, call)| !call.call.state_reverted)
-                    .filter_map(|(log, call)| {
-                        if call.call.address.as_slice() != LIQUIDITY_POOL_ADDRESS.as_slice() {
-                           None
-                        }
-
-                        if let Some(event) = abi::pool_contract::events::Initialized::match_and_decode(log) {
+                    .filter_map(|(log, _)| {
+                        if let Some(_ev) =
+                            abi::pool_contract::events::Initialized::match_and_decode(log)
+                        {
                             Some(
-                                ProtocolComponent::at_contract(&LIQUIDITY_POOL_ADDRESS, tx)
-                                    .with_tokens(&[EETH_ADDRESS, ETH_ADDRESS])
-                                    .as_swap_type("etherfi_liquidity_pool", ImplementationType::Vm),
+                                ProtocolComponent::at_contract(&LIQUIDITY_POOL_ADDRESS, &tx.into())
+                                    .with_tokens(&[EETH_ADDRESS, WEETH_ADDRESS, ETH_ADDRESS])
+                                    .as_swap_type("etherfi_pool", ImplementationType::Vm),
                             )
-                        }
-                        else {
+                        } else {
                             None
                         }
                     })
@@ -83,71 +80,48 @@ pub fn map_relative_balances(
         .logs()
         .flat_map(|log| {
             let mut deltas = Vec::new();
+            let address_bytes_be = log.address();
+            let address_hex = format!("0x{}", hex::encode(address_bytes_be));
 
-            if let Some(event) = abi::dmmpool_contract::events::Mint::match_and_decode(log) {
-                let component_id = address_to_hex(log.address());
+            if let Some(ev) = abi::pool_contract::events::Deposit::match_and_decode(log.log) {
 
-                if let Some((token0, token1)) = maybe_get_pool_tokens(&store, &component_id) {
-                    deltas.extend_from_slice(&[
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token0,
-                            delta: event.amount0.to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token1,
-                            delta: event.amount1.to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                    ]);
+                if store
+                    .get_last(format!("pool:{}", address_hex))
+                    .is_some()
+                {
+                    // Shares are minted, therefore not held by the contract; Contract balance just becomes += ETH
+                    substreams::log::info!("Deposit: -ETH {}", ev.amount);
+
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: ETH_ADDRESS.to_vec(),
+                        delta: ev.amount.to_signed_bytes_be(),
+                        component_id: address_bytes_be.to_vec(),
+                    });
                 }
-            } else if let Some(event) = abi::dmmpool_contract::events::Burn::match_and_decode(log) {
-                let component_id = address_to_hex(log.address());
+            } else if let Some(ev) = abi::pool_contract::events::Withdraw::match_and_decode(log.log)
+            {
+                let address_bytes_be = log.address();
+                let address_hex = format!("0x{}", hex::encode(address_bytes_be));
 
-                if let Some((token0, token1)) = maybe_get_pool_tokens(&store, &component_id) {
-                    deltas.extend_from_slice(&[
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token0,
-                            delta: event.amount0.neg().to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token1,
-                            delta: event.amount1.neg().to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                    ]);
-                }
-            } else if let Some(event) = abi::dmmpool_contract::events::Swap::match_and_decode(log) {
-                let component_id = address_to_hex(log.address());
+                if store
+                    .get_last(format!("pool:{}", address_hex))
+                    .is_some()
+                {
+                    // Shares are minted, therefore not held by the contract; Contract balance just becomes -= ETH
+                    substreams::log::info!("Withdraw: +ETH {}", ev.amount);
 
-                if let Some((token0, token1)) = maybe_get_pool_tokens(&store, &component_id) {
-                    deltas.extend_from_slice(&[
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token0.clone(),
-                            delta: (event.amount0_in - event.amount0_out).to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                        BalanceDelta {
-                            ord: log.ordinal(),
-                            tx: Some(log.receipt.transaction.into()),
-                            token: token1.clone(),
-                            delta: (event.amount1_in - event.amount1_out).to_signed_bytes_be(),
-                            component_id: string_to_bytes(&component_id),
-                        },
-                    ]);
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: ETH_ADDRESS.to_vec(),
+                        delta: ev.amount.neg().to_signed_bytes_be(),
+                        component_id: address_bytes_be.to_vec(),
+                    });
                 }
             }
+
             deltas
         })
         .collect::<Vec<_>>();
@@ -163,35 +137,42 @@ pub fn store_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
 }
 
 /// This is the main map that handles most of the indexing of this substream.
-/// Every contract change is grouped by transaction index via the `transaction_contract_changes`
-///  map. Each block of code will extend the `TransactionContractChanges` struct with the
-///  corresponding changes (balance, component, contract), inserting a new one if it doesn't exist.
+/// Every contract change is grouped by transaction index via the `transaction_changes`
+///  map. Each block of code will extend the `TransactionChanges` struct with the
+///  cooresponding changes (balance, component, contract), inserting a new one if it doesn't exist.
 ///  At the very end, the map can easily be sorted by index to ensure the final
-/// `BlockContractChanges`  is ordered by transactions properly.
+/// `BlockChanges`  is ordered by transactions properly.
 #[substreams::handlers::map]
 pub fn map_protocol_changes(
     block: eth::v2::Block,
     grouped_components: BlockTransactionProtocolComponents,
     deltas: BlockBalanceDeltas,
-    components_store: StoreGetInt64,
+    components_store: StoreGetString,
     balance_store: StoreDeltas, // Note, this map module is using the `deltas` mode for the store.
-) -> Result<BlockContractChanges> {
+) -> Result<BlockChanges, anyhow::Error> {
     // We merge contract changes by transaction (identified by transaction index) making it easy to
     //  sort them at the very end.
-    let mut transaction_contract_changes: HashMap<_, TransactionContractChanges> = HashMap::new();
+    let mut transaction_changes: HashMap<_, TransactionChangesBuilder> = HashMap::new();
 
     // `ProtocolComponents` are gathered from `map_pools_created` which just need a bit of work to
-    //   convert into `TransactionContractChanges`
+    //   convert into `TransactionChanges`
     grouped_components
         .tx_components
         .iter()
         .for_each(|tx_component| {
+            // initialise builder if not yet present for this tx
             let tx = tx_component.tx.as_ref().unwrap();
-            transaction_contract_changes
+            let builder = transaction_changes
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(tx))
-                .component_changes
-                .extend_from_slice(&tx_component.components);
+                .or_insert_with(|| TransactionChangesBuilder::new(tx));
+
+            // iterate over individual components created within this tx
+            tx_component
+                .components
+                .iter()
+                .for_each(|component| {
+                    builder.add_protocol_component(component);
+                });
         });
 
     // Balance changes are gathered by the `StoreDelta` based on `PoolBalanceChanged` creating
@@ -201,45 +182,43 @@ pub fn map_protocol_changes(
     aggregate_balances_changes(balance_store, deltas)
         .into_iter()
         .for_each(|(_, (tx, balances))| {
-            transaction_contract_changes
+            let builder = transaction_changes
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(&tx))
-                .balance_changes
-                .extend(balances.into_values());
+                .or_insert_with(|| TransactionChangesBuilder::new(&tx));
+            balances.values().for_each(|bc| {
+                builder.add_balance_change(bc);
+            });
         });
 
     // Extract and insert any storage changes that happened for any of the components.
-    extract_contract_changes(
+    extract_contract_changes_builder(
         &block,
         |addr| {
             components_store
                 .get_last(format!("pool:0x{0}", hex::encode(addr)))
                 .is_some()
         },
-        &mut transaction_contract_changes,
+        &mut transaction_changes,
     );
 
-    // Process all `transaction_contract_changes` for final output in the `BlockContractChanges`,
+    // Process all `transaction_changes` for final output in the `BlockChanges`,
     //  sorted by transaction index (the key).
-    Ok(BlockContractChanges {
+
+    let block_changes = BlockChanges {
         block: Some((&block).into()),
-        changes: transaction_contract_changes
+        changes: transaction_changes
             .drain()
             .sorted_unstable_by_key(|(index, _)| *index)
-            .filter_map(|(_, change)| {
-                if change.contract_changes.is_empty() &&
-                    change.component_changes.is_empty() &&
-                    change.balance_changes.is_empty()
-                {
-                    None
-                } else {
-                    Some(change)
-                }
-            })
+            .filter_map(|(_, builder)| builder.build())
             .collect::<Vec<_>>(),
-    })
-}
+    };
 
+    for change in &block_changes.changes {
+        substreams::log::info!("🚨 Balance changes {:?}", change.balance_changes);
+        substreams::log::info!("🚨 Component changes {:?}", change.component_changes);
+    }
+    Ok(block_changes)
+}
 fn maybe_get_pool_tokens(
     store: &StoreGetProto<ProtocolComponent>,
     component_id: &str,
