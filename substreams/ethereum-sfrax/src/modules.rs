77c9b0c8e98df5df9acf970f915d55a0e2a24321
Mattia <mp.web3@gmail.com>
2024-10-21 16:49:22 +0200

    updated to support sdk

--- substreams/ethereum-sfrax/src/modules.rs
+++ substreams/ethereum-sfrax/src/modules.rs
@@ -19,7 +19,6 @@ pub fn map_components(
 ) -> Result<BlockTransactionProtocolComponents> {
     let vault_address = hex::decode(params).unwrap();
     let locked_asset = find_deployed_underlying_address(&vault_address).unwrap();
-    // We store these as a hashmap by tx hash since we need to agg by tx hash later
     Ok(BlockTransactionProtocolComponents {
         tx_components: block
             .transactions()
@@ -46,7 +45,6 @@ pub fn map_components(
     })
 }
 
-/// Simply stores the `ProtocolComponent`s with the pool id as the key
 #[substreams::handlers::store]
 pub fn store_components(map: BlockTransactionProtocolComponents, store: StoreAddInt64) {
     store.add_many(
@@ -128,28 +126,6 @@ pub fn map_relative_balances(
                         },
                     ])
                 }
-            } else if let Some(ev) =
-                abi::stakedfrax_contract::events::DistributeRewards::match_and_decode(vault_log.log)
-            {
-                let address_bytes_be = vault_log.address();
-                let address_hex = format!("0x{}", hex::encode(address_bytes_be));
-
-                if store
-                    .get_last(format!("pool:{}", address_hex))
-                    .is_some()
-                {
-                    deltas.push(BalanceDelta {
-                        ord: vault_log.ordinal(),
-                        tx: Some(vault_log.receipt.transaction.into()),
-                        token: find_deployed_underlying_address(address_bytes_be)
-                            .unwrap()
-                            .to_vec(),
-                        delta: ev
-                            .rewards_to_distribute
-                            .to_signed_bytes_be(),
-                        component_id: address_hex.as_bytes().to_vec(),
-                    });
-                }
             }
             deltas
         })
@@ -158,60 +134,43 @@ pub fn map_relative_balances(
     Ok(BlockBalanceDeltas { balance_deltas })
 }
 
-/// It's significant to include both the `pool_id` and the `token_id` for each balance delta as the
-///  store key to ensure that there's a unique balance being tallied for each.
 #[substreams::handlers::store]
 pub fn store_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
     tycho_substreams::balances::store_balance_changes(deltas, store);
 }
 
-/// This is the main map that handles most of the indexing of this substream.
-/// Every contract change is grouped by transaction index via the `transaction_contract_changes`
-///  map. Each block of code will extend the `TransactionContractChanges` struct with the
-///  cooresponding changes (balance, component, contract), inserting a new one if it doesn't exist.
-///  At the very end, the map can easily be sorted by index to ensure the final
-/// `BlockContractChanges`  is ordered by transactions properly.
 #[substreams::handlers::map]
 pub fn map_protocol_changes(
     block: eth::v2::Block,
     grouped_components: BlockTransactionProtocolComponents,
     deltas: BlockBalanceDeltas,
     components_store: StoreGetInt64,
-    balance_store: StoreDeltas, // Note, this map module is using the `deltas` mode for the store.
-) -> Result<BlockContractChanges> {
-    // We merge contract changes by transaction (identified by transaction index) making it easy to
-    //  sort them at the very end.
-    let mut transaction_contract_changes: HashMap<_, TransactionContractChanges> = HashMap::new();
+    balance_store: StoreDeltas,
+) -> Result<BlockChanges, anyhow::Error> {
+    let mut transaction_contract: HashMap<u64, TransactionChanges> = HashMap::new();
 
-    // `ProtocolComponents` are gathered from `map_pools_created` which just need a bit of work to
-    //   convert into `TransactionContractChanges`
     grouped_components
         .tx_components
         .iter()
         .for_each(|tx_component| {
             let tx = tx_component.tx.as_ref().unwrap();
-            transaction_contract_changes
+            transaction_contract
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(tx))
+                .or_insert_with(|| TransactionChanges::new(tx))
                 .component_changes
                 .extend_from_slice(&tx_component.components);
         });
 
-    // Balance changes are gathered by the `StoreDelta` based on `PoolBalanceChanged` creating
-    //  `BlockBalanceDeltas`. We essentially just process the changes that occurred to the `store`
-    // this  block. Then, these balance changes are merged onto the existing map of tx contract
-    // changes,  inserting a new one if it doesn't exist.
     aggregate_balances_changes(balance_store, deltas)
         .into_iter()
         .for_each(|(_, (tx, balances))| {
-            transaction_contract_changes
+            transaction_contract
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(&tx))
+                .or_insert_with(|| TransactionChanges::new(&tx))
                 .balance_changes
                 .extend(balances.into_values());
         });
 
-    // Extract and insert any storage changes that happened for any of the components.
     extract_contract_changes(
         &block,
         |addr| {
@@ -219,14 +178,12 @@ pub fn map_protocol_changes(
                 .get_last(format!("pool:0x{0}", hex::encode(addr)))
                 .is_some()
         },
-        &mut transaction_contract_changes,
+        &mut transaction_contract,
     );
 
-    // Process all `transaction_contract_changes` for final output in the `BlockContractChanges`,
-    //  sorted by transaction index (the key).
-    Ok(BlockContractChanges {
+    Ok(BlockChanges {
         block: Some((&block).into()),
-        changes: transaction_contract_changes
+        changes: transaction_contract
             .drain()
             .sorted_unstable_by_key(|(index, _)| *index)
             .filter_map(|(_, change)| {
@@ -270,33 +227,24 @@ fn create_vault_component(
         .as_swap_type("sfrax_vault", ImplementationType::Vm)
 }
 
-// ref:
-// https://docs.frax.finance/smart-contracts/sfrax-contract-addresses
-// https://docs.frax.finance/smart-contracts/frax
 fn find_deployed_underlying_address(vault_address: &[u8]) -> Option<[u8; 20]> {
     match vault_address {
         hex!("e3b3FE7bcA19cA77Ad877A5Bebab186bEcfAD906") => {
-            // Arbitrum
             Some(hex!("e3b3FE7bcA19cA77Ad877A5Bebab186bEcfAD906"))
         }
         hex!("a63f56985F9C7F3bc9fFc5685535649e0C1a55f3") => {
-            // BSC
             Some(hex!("a63f56985F9C7F3bc9fFc5685535649e0C1a55f3"))
         }
         hex!("A663B02CF0a4b149d2aD41910CB81e23e1c41c32") => {
-            // Ethereum
             Some(hex!("A663B02CF0a4b149d2aD41910CB81e23e1c41c32"))
         }
         hex!("3405E88af759992937b84E58F2Fe691EF0EeA320") => {
-            // Avalanche
             Some(hex!("3405E88af759992937b84E58F2Fe691EF0EeA320"))
         }
         hex!("2Dd1B4D4548aCCeA497050619965f91f78b3b532") => {
-            // Optimism
             Some(hex!("2Dd1B4D4548aCCeA497050619965f91f78b3b532"))
         }
         hex!("2C37fb628b35dfdFD515d41B0cAAe11B542773C3") => {
-            // Polygon
             Some(hex!("2C37fb628b35dfdFD515d41B0cAAe11B542773C3"))
         }
         _ => None,
