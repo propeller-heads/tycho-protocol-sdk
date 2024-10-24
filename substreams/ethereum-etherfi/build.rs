e4f1fe8bb86f1f23d95fb5592b23529c19e41932
Mattia <mp.web3@gmail.com>
2024-10-24 11:26:54 +0200

    fix: fixed blocks to fetch and formatted

--- substreams/ethereum-etherfi/build.rs
+++ substreams/ethereum-etherfi/build.rs
@@ -1,24 +1,17 @@
 use anyhow::{Ok, Result};
 use regex::Regex;
-use substreams_ethereum::Abigen;
 use std::fs;
+use substreams_ethereum::Abigen;
 
 fn main() -> Result<(), anyhow::Error> {
-    let file_names = [
-        "abi/pool_contract.abi.json",
-        "abi/eeth_contract.abi.json",
-        "abi/erc20.abi.json",
-    ];
-    let file_output_names = [
-        "src/abi/pool_contract.rs",
-        "src/abi/eeth_contract.rs",
-        "src/abi/erc20.rs",
-    ];
+    let file_names =
+        ["abi/pool_contract.abi.json", "abi/eeth_contract.abi.json", "abi/erc20.abi.json"];
+    let file_output_names =
+        ["src/abi/pool_contract.rs", "src/abi/eeth_contract.rs", "src/abi/erc20.rs"];
 
     let mut i = 0;
     for f in file_names {
-        let contents = fs::read_to_string(f)
-            .expect("Should have been able to read the file");
+        let contents = fs::read_to_string(f).expect("Should have been able to read the file");
 
         // sanitize fields and attributes starting with an underscore
         let regex = Regex::new(r#"("\w+"\s?:\s?")_(\w+")"#).unwrap();
@@ -28,7 +21,7 @@ fn main() -> Result<(), anyhow::Error> {
             .generate()?
             .write_to_file(file_output_names[i])?;
 
-        i = i+1;
+        i = i + 1;
     }
 
     Ok(())
