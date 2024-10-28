c7048acad7d7baa0724f93d008046873bebe9143
Mattia <mp.web3@gmail.com>
2024-10-28 16:36:40 +0100

    cargo.toml and rust-toolchain.toml update

--- substreams/ethereum-sdai/src/pb/mod.rs
+++ substreams/ethereum-sdai/src/pb/mod.rs
@@ -1,12 +1,4 @@
-#[allow(clippy::all)]
 // @generated
-pub mod contract {
-    // @@protoc_insertion_point(attribute:contract.v1)
-    pub mod v1 {
-        include!("contract.v1.rs");
-        // @@protoc_insertion_point(contract.v1)
-    }
-}
 pub mod tycho {
     pub mod evm {
         // @@protoc_insertion_point(attribute:tycho.evm.v1)
