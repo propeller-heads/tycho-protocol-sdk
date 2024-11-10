939098b73d8218a7242908b0b1326883e92b56c0
Mattia <mp.web3@gmail.com>
2024-11-10 12:33:15 +0100

    fixes

--- substreams/ethereum-bancor/src/modules.rs
+++ substreams/ethereum-bancor/src/modules.rs
@@ -6,7 +6,7 @@ use substreams::{
     hex,
     pb::substreams::StoreDeltas,
     store::{
-        StoreAddBigInt, StoreGet, StoreGetInt64, StoreGetProto, StoreNew, StoreSet, StoreSetString,
+        StoreAddBigInt, StoreGet, StoreGetInt64, StoreGetProto, StoreNew, StoreSet, StoreSetString, StoreGetString
     },
 };
 use substreams_ethereum::{
@@ -23,40 +23,65 @@ use tycho_substreams::{
 pub const FACTORY_ADDRESS: [u8; 20] = hex!("eEF417e1D5CC832e619ae18D2F140De2999dD4fB");
 pub const BNT_ADDRESS: [u8; 20] = hex!("1F573D6Fb3F13d689FF844B4cE37794d79a7FF1C");
 
-#[substreams::handlers::map]
-pub fn map_components(
-    block: eth::v2::Block,
-) -> Result<BlockTransactionProtocolComponents, anyhow::Error> {
-    Ok(BlockTransactionProtocolComponents {
-        tx_components: block
-            .transactions()
-            .filter_map(|tx| {
-                let components = tx
-                    .logs_with_calls()
-                    .filter(|(_, call)| !call.call.state_reverted)
-                    .filter_map(|(log, _)| address_map(&log, &tx))
-                    .collect::<Vec<_>>();
 
+// Define a map handler function named `map_components` using Substreams
+#[substreams::handlers::map]
+// Purpose: Map Ethereum blocks to components by filtering out transactions that are reverted and
+// creating a collection of protocol components.
+pub fn map_components(
+    block: eth::v2::Block,  // Take in an Ethereum block as input
+) -> Result<BlockTransactionProtocolComponents, anyhow::Error> { // Return a result with protocol components or an error
+
+    // Return a successful result containing a BlockTransactionProtocolComponents struct
+    Ok(BlockTransactionProtocolComponents {
+        // Process each transaction in the block to find components related to the protocol
+        tx_components: block
+            .transactions()  // Access the list of transactions within the block
+            .filter_map(|tx| {  // Use filter_map to collect only transactions with relevant components
+
+                // For each transaction, gather logs and call data to find components
+                let components = tx
+                    .logs_with_calls()  // Get logs along with call details for each transaction
+                    .filter(|(_, call)| !call.call.state_reverted)  // Filter out logs where the call was reverted
+                    .filter_map(|(log, _)| address_map(&log, &tx))  // Map logs to protocol components, skipping if `address_map` returns None
+                    .collect::<Vec<_>>();  // Collect all components for this transaction into a vector
+
+                // If we found any components, return a TransactionProtocolComponents struct for this transaction
                 if !components.is_empty() {
-                    Some(TransactionProtocolComponents { tx: Some(tx.into()), components })
+                    Some(TransactionProtocolComponents { 
+                        tx: Some(tx.into()),  // Convert the transaction data into the expected format
+                        components  // Attach the vector of components found for this transaction
+                    })
                 } else {
-                    None
+                    None  // If no components were found, return None for this transaction
                 }
             })
-            .collect::<Vec<_>>(),
+            .collect::<Vec<_>>(),  // Collect all transactions with components into a vector
     })
 }
 
+
 /// Simply stores the `ProtocolComponent`s with the pool address as the key and the pool id as value
 #[substreams::handlers::store]
-pub fn store_components(map: BlockTransactionProtocolComponents, store: StoreSetString) {
-    map.tx_components
-        .into_iter()
-        .for_each(|tx_pc| {
+pub fn store_components(
+    map: BlockTransactionProtocolComponents,  // Input: contains transactions with their components
+    store: StoreSetString                     // Store interface for setting string values
+) {
+    map.tx_components // Get all transactions that have components
+        .into_iter() // Convert into an iterator
+        .for_each(|tx_pc| { // For each transaction with components...
             tx_pc
-                .components
+                .components // Get the components from this transaction
                 .into_iter()
-                .for_each(|pc| store.set(0, format!("pool:{0}", &pc.id[..42]), &pc.id))
+                .for_each(|pc| { // For each component
+                    let key = format!("pool:{0}", &pc.id[..42]);
+                    substreams::log::info!("Storing component with key: {}, value: {}", key, &pc.id);
+                    store.set(
+                        0, // Starting ordinal (always 0 in this case)
+                        format!("pool:{0}", &pc.id[..42]), // Key: "pool:" prefix + first 42 chars of component ID
+                        &pc.id // Value: the full component ID
+                    )
+                });
         });
 }
 
@@ -65,7 +90,7 @@ pub fn store_components(map: BlockTransactionProtocolComponents, store: StoreSet
 #[substreams::handlers::map]
 pub fn map_relative_balances(
     block: eth::v2::Block,
-    store: StoreGetProto<ProtocolComponent>,
+    store: StoreGetString
 ) -> Result<BlockBalanceDeltas, anyhow::Error> {
     let balance_deltas = block
         .logs()
@@ -131,7 +156,7 @@ pub fn map_protocol_changes(
     block: eth::v2::Block,
     grouped_components: BlockTransactionProtocolComponents,
     deltas: BlockBalanceDeltas,
-    components_store: StoreGetInt64,
+    components_store: StoreGetString,
     balance_store: StoreDeltas, // Note, this map module is using the `deltas` mode for the store.
 ) -> Result<BlockChanges, anyhow::Error> {
     // We merge contract changes by transaction (identified by transaction index) making it easy to
@@ -211,17 +236,39 @@ fn address_to_hex(address: &[u8]) -> String {
     format!("0x{}", hex::encode(address))
 }
 
+// Define a function `address_map` that takes a reference to a log and transaction trace
 fn address_map(log: &Log, tx: &TransactionTrace) -> Option<ProtocolComponent> {
+    
+    // Clone the address from the log for comparison
+    // Log.address is the address of the contract that emitted the event
     let address = log.address.to_owned();
-    if address == FACTORY_ADDRESS {
-        PoolAdded::match_and_decode(log).map(|pool_added| {
-            substreams::log::info!("🚨 Pool added 0x{:?}", hex::encode(&pool_added.pool));
 
+    // Check if the address matches the specific `FACTORY_ADDRESS`
+    if address == FACTORY_ADDRESS {
+
+        // Checks if the log matches the event signature of PoolAdded
+        // If it matches, decodes the log data into the PoolAdded struct
+        // .map(|pool_added| { ... }) is using Rust's Option combinators:
+        // match_and_decode returns an Option<PoolAdded>
+        // .map() will only execute if the Option is Some
+        // The closure |pool_added| receives the decoded PoolAdded event data
+        PoolAdded::match_and_decode(log).map(|pool_added| {
+
+            // Log information about the new pool that was added, displaying the pool's address
+            substreams::log::info!("🏦 Pool added for token: 0x{}", hex::encode(&pool_added.pool));
+
+            // Construct a `ProtocolComponent` at the `pool_added` address with the transaction data
             ProtocolComponent::at_contract(&pool_added.pool, &(tx.into()))
+                
+                // Specify the tokens related to this pool, which include `pool_added.pool` and the `BNT_ADDRESS`
                 .with_tokens(&[pool_added.pool, BNT_ADDRESS.to_vec()])
+                
+                // Set the component type to "bancor_v3_pool" with a swap type of "Vm"
                 .as_swap_type("bancor_v3_pool", ImplementationType::Vm)
         })
     } else {
+        // If the address does not match `FACTORY_ADDRESS`, return None
         None
     }
 }
+
