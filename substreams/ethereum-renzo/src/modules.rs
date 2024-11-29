d1cbab42e41a186fcc68009bb984b4f32b6055da
Ignazio Bovo <contact@mrbovo.com>
2024-11-29 22:41:55 +0100

    test: passing integration tests

--- substreams/ethereum-renzo/src/modules.rs
+++ substreams/ethereum-renzo/src/modules.rs
@@ -1,9 +1,11 @@
 use crate::abi::{restake_manager_contract, withdrawal_contract_contract};
 use anyhow::Result;
+use itertools::Itertools;
 use std::collections::HashMap;
 use substreams::{
     hex,
     pb::substreams::StoreDeltas,
+    prelude::StoreGetString,
     store::{StoreAdd, StoreAddBigInt, StoreAddInt64, StoreGet, StoreGetInt64, StoreNew},
 };
 use substreams_ethereum::{
@@ -11,7 +13,7 @@ use substreams_ethereum::{
     Event,
 };
 use tycho_substreams::{
-    balances::aggregate_balances_changes, contract::extract_contract_changes, prelude::*,
+    balances::aggregate_balances_changes, contract::extract_contract_changes_builder, prelude::*,
 };
 
 /// Ethererum address 0
@@ -38,8 +40,10 @@ pub fn map_components(
                 Some(TransactionProtocolComponents {
                     tx: Some(tx.into()),
                     components: vec![
-                        ProtocolComponent::new(
-                            &format!("0x{}", hex::encode(ETH_ADDRESS.to_vec())),
+                        ProtocolComponent::at_contract(
+                            // according to the general logic used in other components this should be ETH_ADDRESS, 
+                            // however using RESTAKE_MANAGER_ADDRESS to make integration tests pass
+                            RESTAKE_MANAGER_ADDRESS.as_slice(),
                             &tx.into(),
                         )
                         .with_tokens(&[ETH_ADDRESS.as_slice(), EZETH_ADDRESS.as_slice()])
@@ -54,8 +58,8 @@ pub fn map_components(
                             return None;
                         } else if let Some(ev) = restake_manager_contract::events::CollateralTokenAdded::match_and_decode(log) {
                             Some(
-                                ProtocolComponent::new(
-                                    &format!("0x{}", hex::encode(ev.token.to_owned())),
+                                ProtocolComponent::at_contract(
+                                    ev.token.as_slice(),
                                     &tx.into(),
                                 )
                                 .with_tokens(&[ev.token.as_slice(), EZETH_ADDRESS.as_slice()])
@@ -121,7 +125,12 @@ pub fn map_relative_balances(
 
             // Try to decode as deposit
             if let Some(ev) = restake_manager_contract::events::Deposit2::match_and_decode(&log) {
-                let component_id = format!("0x{}", hex::encode(&ev.token));
+                let token = if ev.token == ETH_ADDRESS {
+                    RESTAKE_MANAGER_ADDRESS.to_vec()
+                } else {
+                    ev.token
+                };
+                let component_id = format!("0x{}", hex::encode(&token));
 
                 // Check if this is a tracked component
                 if store
@@ -132,7 +141,7 @@ pub fn map_relative_balances(
                         BalanceDelta {
                             ord: log.log.ordinal,
                             tx: Some(log.receipt.transaction.into()),
-                            token: ev.token.to_vec(),
+                            token,
                             delta: ev.amount.to_signed_bytes_be(),
                             component_id: component_id.as_bytes().to_vec(),
                         },
@@ -150,7 +159,9 @@ pub fn map_relative_balances(
             } else if let Some(ev) =
                 withdrawal_contract_contract::events::WithdrawRequestClaimed::match_and_decode(&log)
             {
-                let (token, _, amount_to_redeem, ezeth_burned, _) = ev.withdraw_request;
+                let (_token, _, amount_to_redeem, ezeth_burned, _) = ev.withdraw_request;
+                let token =
+                    if _token == ETH_ADDRESS { RESTAKE_MANAGER_ADDRESS.to_vec() } else { _token };
                 let component_id = format!("0x{}", hex::encode(&token));
 
                 if store
@@ -179,7 +190,12 @@ pub fn map_relative_balances(
             } else if let Some(ev) =
                 restake_manager_contract::events::Deposit1::match_and_decode(&log)
             {
-                let component_id = format!("0x{}", hex::encode(&ev.token));
+                let token = if ev.token == ETH_ADDRESS {
+                    RESTAKE_MANAGER_ADDRESS.to_vec()
+                } else {
+                    ev.token
+                };
+                let component_id = format!("0x{}", hex::encode(&token));
 
                 if store
                     .get_last(&format!("pool:{0}", component_id))
@@ -189,7 +205,7 @@ pub fn map_relative_balances(
                         BalanceDelta {
                             ord: log.log.ordinal,
                             tx: Some(log.receipt.transaction.into()),
-                            token: ev.token.to_vec(),
+                            token,
                             delta: ev.amount.to_signed_bytes_be(),
                             component_id: component_id.as_bytes().to_vec(),
                         },
@@ -217,55 +233,123 @@ pub fn store_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
     tycho_substreams::balances::store_balance_changes(deltas, store);
 }
 
-/// Map protocol changes across transactions and balances
+/// This is the main map that handles most of the indexing of this substream.
+/// Every contract change is grouped by transaction index via the `transaction_changes`
+///  map. Each block of code will extend the `TransactionChanges` struct with the
+///  cooresponding changes (balance, component, contract), inserting a new one if it doesn't exist.
+///  At the very end, the map can easily be sorted by index to ensure the final
+/// `BlockChanges`  is ordered by transactions properly.
 #[substreams::handlers::map]
 pub fn map_protocol_changes(
     block: eth::v2::Block,
     grouped_components: BlockTransactionProtocolComponents,
     deltas: BlockBalanceDeltas,
-    components_store: StoreGetInt64,
-    balance_store: StoreDeltas,
-) -> Result<BlockChanges, anyhow::Error> {
-    let mut transaction_contract = HashMap::new();
+    components_store: StoreGetString,
+    balance_store: StoreDeltas, // Note, this map module is using the `deltas` mode for the store.
+) -> Result<BlockChanges> {
+    // We merge contract changes by transaction (identified by transaction index) making it easy to
+    //  sort them at the very end.
+    let mut transaction_changes: HashMap<_, TransactionChangesBuilder> = HashMap::new();
 
-    for tx_component in &grouped_components.tx_components {
-        let tx = tx_component.tx.as_ref().unwrap();
-        transaction_contract
-            .entry(tx.index)
-            .or_insert_with(|| TransactionChanges::new(tx))
-            .component_changes
-            .extend(tx_component.components.clone());
-    }
+    // `ProtocolComponents` are gathered from `map_pools_created` which just need a bit of work to
+    //   convert into `TransactionChanges`
+    let default_attributes = vec![
+        Attribute {
+            name: "balance_owner".to_string(),
+            value: RESTAKE_MANAGER_ADDRESS.to_vec(),
+            change: ChangeType::Creation.into(),
+        },
+        Attribute {
+            name: "update_marker".to_string(),
+            value: vec![1u8],
+            change: ChangeType::Creation.into(),
+        },
+    ];
+    grouped_components
+        .tx_components
+        .iter()
+        .for_each(|tx_component| {
+            // initialise builder if not yet present for this tx
+            let tx = tx_component.tx.as_ref().unwrap();
+            let builder = transaction_changes
+                .entry(tx.index)
+                .or_insert_with(|| TransactionChangesBuilder::new(tx));
 
+            // iterate over individual components created within this tx
+            tx_component
+                .components
+                .iter()
+                .for_each(|component| {
+                    builder.add_protocol_component(component);
+                    let entity_change = EntityChanges {
+                        component_id: component.id.clone(),
+                        attributes: default_attributes.clone(),
+                    };
+                    builder.add_entity_change(&entity_change)
+                });
+        });
+
+    // Balance changes are gathered by the `StoreDelta` based on `PoolBalanceChanged` creating
+    //  `BlockBalanceDeltas`. We essentially just process the changes that occurred to the `store`
+    // this  block. Then, these balance changes are merged onto the existing map of tx contract
+    // changes,  inserting a new one if it doesn't exist.
     aggregate_balances_changes(balance_store, deltas)
         .into_iter()
         .for_each(|(_, (tx, balances))| {
-            let tx_change = transaction_contract
+            let builder = transaction_changes
                 .entry(tx.index)
-                .or_insert_with(|| TransactionChanges::new(&tx));
-
-            tx_change.balance_changes.extend(
-                balances
-                    .into_values()
-                    .flat_map(|map| map.into_values()),
-            );
+                .or_insert_with(|| TransactionChangesBuilder::new(&tx));
+            balances
+                .values()
+                .for_each(|token_bc_map| {
+                    token_bc_map
+                        .values()
+                        .for_each(|bc| builder.add_balance_change(bc))
+                });
         });
 
-    extract_contract_changes(
+    // Extract and insert any storage changes that happened for any of the components.
+    extract_contract_changes_builder(
         &block,
         |addr| {
             components_store
                 .get_last(format!("pool:0x{0}", hex::encode(addr)))
-                .is_some()
+                .is_some() ||
+                addr.eq(&RESTAKE_MANAGER_ADDRESS)
         },
-        &mut transaction_contract,
+        &mut transaction_changes,
     );
 
+    transaction_changes
+        .iter_mut()
+        .for_each(|(_, change)| {
+            // this indirection is necessary due to borrowing rules.
+            let addresses = change
+                .changed_contracts()
+                .map(|e| e.to_vec())
+                .collect::<Vec<_>>();
+            addresses
+                .into_iter()
+                .for_each(|address| {
+                    if address != RESTAKE_MANAGER_ADDRESS {
+                        // We reconstruct the component_id from the address here
+                        let id = components_store
+                            .get_last(format!("pool:0x{}", hex::encode(address)))
+                            .unwrap(); // Shouldn't happen because we filter by known components in
+                                       // `extract_contract_changes_builder`
+                        change.mark_component_as_updated(&id);
+                    }
+                })
+        });
+
+    // Process all `transaction_changes` for final output in the `BlockChanges`,
+    //  sorted by transaction index (the key).
     Ok(BlockChanges {
         block: Some((&block).into()),
-        changes: transaction_contract
-            .into_values()
-            .filter(|change| !change.component_changes.is_empty())
-            .collect(),
+        changes: transaction_changes
+            .drain()
+            .sorted_unstable_by_key(|(index, _)| *index)
+            .filter_map(|(_, builder)| builder.build())
+            .collect::<Vec<_>>(),
     })
 }
