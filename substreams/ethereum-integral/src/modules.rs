542fbc5f3aae6e3068c876ea3fe7bd4d5fb2e0bd
0xMochan <mochanbot@gmail.com>
2024-12-05 15:20:33 -0800

    feat: implement integral substream

--- substreams/ethereum-integral/src/modules.rs
+++ substreams/ethereum-integral/src/modules.rs
@@ -5,11 +5,11 @@ use std::collections::HashMap;
 use substreams::{
     hex,
     pb::substreams::StoreDeltas,
-    store::{StoreAdd, StoreAddBigInt, StoreAddInt64, StoreGet, StoreGetInt64, StoreNew},
+    store::{StoreAddBigInt, StoreGet, StoreGetString, StoreNew, StoreSet as _, StoreSetString},
 };
 use substreams_ethereum::{pb::eth, Event, Function};
 use tycho_substreams::{
-    balances::aggregate_balances_changes, contract::extract_contract_changes, prelude::*,
+    balances::aggregate_balances_changes, contract::extract_contract_changes_builder, prelude::*,
 };
 
 const RELAYER_PROXY: &[u8] = &hex!("d17b3c9784510E33cD5B87b490E79253BcD81e2E");
@@ -71,74 +71,131 @@ pub fn map_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolC
     })
 }
 
-/// Simply stores the `ProtocolComponent`s with the pool id as the key
+/// Get result `map_components` and stores the created `ProtocolComponent`s with the pool id as the
+/// key and tokens as the value
 #[substreams::handlers::store]
-pub fn store_components(map: BlockTransactionProtocolComponents, store: StoreAddInt64) {
-    store.add_many(
-        0,
-        &map.tx_components
-            .iter()
-            .flat_map(|tx_components| &tx_components.components)
-            .map(|component| format!("pool:{0}", component.id))
-            .collect::<Vec<_>>(),
-        1,
-    );
+pub fn store_component_tokens(map: BlockChanges, store: StoreSetString) {
+    map.changes
+        .iter()
+        .flat_map(|tx_changes| &tx_changes.component_changes)
+        .for_each(|component| {
+            store.set(
+                0,
+                format!("pool:{0}", component.id),
+                &component
+                    .tokens
+                    .iter()
+                    .map(hex::encode)
+                    .join(":"),
+            );
+        });
 }
 
 /// Since the `PoolBalanceChanged` and `Swap` events administer only deltas, we need to leverage a
-/// map and a  store to be able to tally up final balances for tokens in a pool.
+///  map and a store to be able to tally up final balances for tokens in a pool.
 #[substreams::handlers::map]
 pub fn map_relative_balances(
     block: eth::v2::Block,
-    store: StoreGetInt64,
+    store: StoreGetString,
 ) -> Result<BlockBalanceDeltas, anyhow::Error> {
     let balance_deltas = block
         .logs()
-        .filter(|log| log.address() == RELAYER_PROXY)
-        .flat_map(|vault_log| {
+        .filter_map(|log| {
             let mut deltas = Vec::new();
+            let pool_tokens = store.get_first(format!("pool:{0}", hex::encode(log.address())))?;
+            let tokens = pool_tokens
+                .split(':')
+                .map(|token| hex::decode(token).unwrap())
+                .collect::<Vec<_>>();
 
-            if store
-                .get_last(format!("pool:{}", hex::encode(RELAYER_PROXY)))
-                .is_none()
-            {
-                return vec![]
-            }
-
-            if let Some(ev) = abi::relayer::events::Sell::match_and_decode(vault_log.log) {
+            if let Some(ev) = abi::relayer::events::Sell::match_and_decode(log.log) {
                 deltas.push(BalanceDelta {
-                    ord: vault_log.ordinal(),
-                    tx: Some(vault_log.receipt.transaction.into()),
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
                     token: ev.token_in.to_vec(),
                     delta: ev.amount_in.to_signed_bytes_be(),
                     component_id: RELAYER_PROXY.to_vec(),
                 });
                 deltas.push(BalanceDelta {
-                    ord: vault_log.ordinal(),
-                    tx: Some(vault_log.receipt.transaction.into()),
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
                     token: ev.token_out.to_vec(),
                     delta: ev.amount_out.neg().to_signed_bytes_be(),
                     component_id: RELAYER_PROXY.to_vec(),
                 });
-            } else if let Some(ev) = abi::relayer::events::Buy::match_and_decode(vault_log.log) {
+            } else if let Some(ev) = abi::relayer::events::Buy::match_and_decode(log.log) {
                 deltas.push(BalanceDelta {
-                    ord: vault_log.ordinal(),
-                    tx: Some(vault_log.receipt.transaction.into()),
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
                     token: ev.token_in.to_vec(),
                     delta: ev.amount_in.to_signed_bytes_be(),
                     component_id: RELAYER_PROXY.to_vec(),
                 });
                 deltas.push(BalanceDelta {
-                    ord: vault_log.ordinal(),
-                    tx: Some(vault_log.receipt.transaction.into()),
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
                     token: ev.token_out.to_vec(),
                     delta: ev.amount_out.neg().to_signed_bytes_be(),
                     component_id: RELAYER_PROXY.to_vec(),
                 });
+            } else if let Some(ev) = abi::relayer::events::Buy::match_and_decode(log.log) {
+                deltas.push(BalanceDelta {
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
+                    token: ev.token_in.to_vec(),
+                    delta: ev.amount_in.to_signed_bytes_be(),
+                    component_id: RELAYER_PROXY.to_vec(),
+                });
+                deltas.push(BalanceDelta {
+                    ord: log.ordinal(),
+                    tx: Some(log.receipt.transaction.into()),
+                    token: ev.token_out.to_vec(),
+                    delta: ev.amount_out.neg().to_signed_bytes_be(),
+                    component_id: RELAYER_PROXY.to_vec(),
+                });
+            } else if let Some(swap) = abi::pair::events::Swap::match_and_decode(log.log) {
+                if swap.amount1_in.is_zero() {
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: tokens[0].clone(),
+                        delta: swap.amount0_in.to_signed_bytes_be(),
+                        component_id: log.address().to_vec(),
+                    });
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: tokens[1].clone(),
+                        delta: swap
+                            .amount1_out
+                            .neg()
+                            .to_signed_bytes_be(),
+                        component_id: log.address().to_vec(),
+                    });
+                } else if swap.amount0_in.is_zero() {
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: tokens[1].clone(),
+                        delta: swap.amount0_in.to_signed_bytes_be(),
+                        component_id: log.address().to_vec(),
+                    });
+                    deltas.push(BalanceDelta {
+                        ord: log.ordinal(),
+                        tx: Some(log.receipt.transaction.into()),
+                        token: tokens[0].clone(),
+                        delta: swap
+                            .amount1_out
+                            .neg()
+                            .to_signed_bytes_be(),
+                        component_id: log.address().to_vec(),
+                    });
+                }
             }
 
-            deltas
+            Some(deltas)
         })
+        .flatten()
         .collect::<Vec<_>>();
 
     Ok(BlockBalanceDeltas { balance_deltas })
@@ -153,78 +210,87 @@ pub fn store_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
 
 /// This is the main map that handles most of the indexing of this substream.
 /// Every contract change is grouped by transaction index via the `transaction_contract_changes`
-///  map. Each block of code will extend the `TransactionContractChanges` struct with the
+///  map. Each block of code will extend the `TransactionChanges` struct with the
 ///  cooresponding changes (balance, component, contract), inserting a new one if it doesn't exist.
 ///  At the very end, the map can easily be sorted by index to ensure the final
-/// `BlockContractChanges`  is ordered by transactions properly.
+/// `BlockChanges` is ordered by transactions properly.
 #[substreams::handlers::map]
 pub fn map_protocol_changes(
     block: eth::v2::Block,
     grouped_components: BlockTransactionProtocolComponents,
     deltas: BlockBalanceDeltas,
-    components_store: StoreGetInt64,
+    components_store: StoreGetString,
     balance_store: StoreDeltas, // Note, this map module is using the `deltas` mode for the store.
-) -> Result<BlockContractChanges> {
+) -> Result<BlockChanges> {
     // We merge contract changes by transaction (identified by transaction index) making it easy to
     //  sort them at the very end.
-    let mut transaction_contract_changes: HashMap<_, TransactionContractChanges> = HashMap::new();
+    let mut transaction_changes: HashMap<_, TransactionChangesBuilder> = HashMap::new();
 
     // `ProtocolComponents` are gathered from `map_pools_created` which just need a bit of work to
-    //   convert into `TransactionContractChanges`
+    //   convert into `TransactionChanges`
     grouped_components
         .tx_components
         .iter()
         .for_each(|tx_component| {
+            // initialise builder if not yet present for this tx
             let tx = tx_component.tx.as_ref().unwrap();
-            transaction_contract_changes
+            let builder = transaction_changes
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(tx))
-                .component_changes
-                .extend_from_slice(&tx_component.components);
+                .or_insert_with(|| TransactionChangesBuilder::new(tx));
+
+            // iterate over individual components created within this tx
+            tx_component
+                .components
+                .iter()
+                .for_each(|component| {
+                    builder.add_protocol_component(component);
+                    let entity_change = EntityChanges {
+                        component_id: component.id.clone(),
+                        attributes: component.static_att.clone(),
+                    };
+                    builder.add_entity_change(&entity_change)
+                });
         });
 
     // Balance changes are gathered by the `StoreDelta` based on `PoolBalanceChanged` creating
     //  `BlockBalanceDeltas`. We essentially just process the changes that occurred to the `store`
-    // this  block. Then, these balance changes are merged onto the existing map of tx contract
-    // changes,  inserting a new one if it doesn't exist.
+    // this block. Then, these balance changes are merged onto the existing map of tx contract
+    // changes, inserting a new one if it doesn't exist.
     aggregate_balances_changes(balance_store, deltas)
         .into_iter()
         .for_each(|(_, (tx, balances))| {
-            transaction_contract_changes
+            let builder = transaction_changes
                 .entry(tx.index)
-                .or_insert_with(|| TransactionContractChanges::new(&tx))
-                .balance_changes
-                .extend(balances.into_values());
+                .or_insert_with(|| TransactionChangesBuilder::new(&tx));
+            balances
+                .values()
+                .for_each(|token_bc_map| {
+                    token_bc_map
+                        .values()
+                        .for_each(|bc| builder.add_balance_change(bc))
+                });
         });
 
     // Extract and insert any storage changes that happened for any of the components.
-    extract_contract_changes(
+    extract_contract_changes_builder(
         &block,
         |addr| {
             components_store
                 .get_last(format!("pool:0x{0}", hex::encode(addr)))
                 .is_some()
+                || addr.eq(RELAYER_PROXY)
         },
-        &mut transaction_contract_changes,
+        &mut transaction_changes,
     );
 
-    // Process all `transaction_contract_changes` for final output in the `BlockContractChanges`,
+    // Process all `transaction_changes` for final output in the `BlockChanges`,
     //  sorted by transaction index (the key).
-    Ok(BlockContractChanges {
+    Ok(BlockChanges {
         block: Some((&block).into()),
-        changes: transaction_contract_changes
+        changes: transaction_changes
             .drain()
             .sorted_unstable_by_key(|(index, _)| *index)
-            .filter_map(|(_, change)| {
-                if change.contract_changes.is_empty() &&
-                    change.component_changes.is_empty() &&
-                    change.balance_changes.is_empty()
-                {
-                    None
-                } else {
-                    Some(change)
-                }
-            })
+            .filter_map(|(_, builder)| builder.build())
             .collect::<Vec<_>>(),
     })
 }
