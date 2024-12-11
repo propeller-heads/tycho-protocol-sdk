768235b143c9493f88e844f1797ec58a7ea844ee
Thales <thales@datarevenue.com>
2024-12-10 23:09:55 -0300

    feat: initial data structure + ABI & buf models

---
+++ substreams/ethereum-uniswap-v4/src/pb/mod.rs
@@ -0,0 +1,17 @@
+// @generated
+pub mod tycho {
+    pub mod evm {
+        // @@protoc_insertion_point(attribute:tycho.evm.v1)
+        pub mod v1 {
+            include!("tycho.evm.v1.rs");
+            // @@protoc_insertion_point(tycho.evm.v1)
+        }
+    }
+}
+pub mod uniswap {
+    // @@protoc_insertion_point(attribute:uniswap.v4)
+    pub mod v4 {
+        include!("uniswap.v4.rs");
+        // @@protoc_insertion_point(uniswap.v4)
+    }
+}
