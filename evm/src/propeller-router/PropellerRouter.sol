1091591dfbbce21ae4578f18fbd033c16b82fb8a
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2024-12-20 14:30:27 -0500

    refactor: collapse internal and external router contract
    - The internal was useful in the case where we would want to reuse the methods for several different routers (i.e. Fusion, UniswapX, Cowswap, and Frontend). Now - this is not the case for the forseeable future. So, let's keep this as simple as possible and avoid premature overgeneralization.

--- evm/src/propeller-router/PropellerRouter.sol
+++ evm/src/propeller-router/PropellerRouter.sol
@@ -5,20 +5,20 @@ import "@interfaces/batch-swap-router/ICowSwapRouterPublic.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@src/libraries/PackedSwapStructs.sol";
 import "@src/libraries/EfficientERC20.sol";
-import "./PropellerRouterInternal.sol";
 import "./SwapExecutionDispatcher.sol";
 import "./CallbackVerificationDispatcher.sol";
 import "./ApprovalManagement.sol";
 import "./SwapContext.sol";
 
 contract PropellerRouter is
-    PropellerRouterInternal,
+    PropellerRouterStructs,
     SwapContext,
     SwapExecutionDispatcher,
     CallbackVerificationDispatcher,
     ApprovalManagement,
     AccessControl
 {
+    using PrefixLengthEncodedByteArray for bytes;
     using PackedSwapStructs for bytes;
     using EfficientERC20 for IERC20;
 
@@ -55,6 +55,17 @@ contract PropellerRouter is
             _callVerifyCallback(data);
     }
 
+    /**
+     * @dev Executes a single swap, but might do more actions through callbacks.
+     */
+    function _singleSwap(uint256 amount, bytes calldata swap)
+        internal
+        returns (uint256 calculatedAmount)
+    {
+        calculatedAmount =
+            _executeSwap(swap.exchange(), amount, swap.protocolData());
+    }
+
     function _quoteSwap(
         uint8 exchange,
         uint256 amount,
@@ -73,7 +84,20 @@ contract PropellerRouter is
         withSwapContext
         returns (uint256 calculatedAmount)
     {
-        calculatedAmount = _singleExactIn(givenAmount, swap);
+        (
+            uint256 minUserAmount,
+            IERC20 tokenOut,
+            address receiver,
+            bytes calldata swap
+        ) = data.decodeSingleCheckedArgs();
+        // We need to measure spent amount via balanceOf, as
+        // callbacks might execute additional swaps
+        uint256 balanceBefore = tokenOut.balanceOf(receiver);
+        _singleSwap(givenAmount, swap);
+        calculatedAmount = tokenOut.balanceOf(receiver) - balanceBefore;
+        if (calculatedAmount < minUserAmount) {
+            revert NegativeSlippage(calculatedAmount, minUserAmount);
+        }
     }
 
     function singleExactOut(uint256 givenAmount, bytes calldata swap)
@@ -83,9 +107,26 @@ contract PropellerRouter is
         withSwapContext
         returns (uint256 calculatedAmount)
     {
-        calculatedAmount = _singleExactOut(givenAmount, swap);
+        (
+            uint256 maxUserAmount,
+            IERC20 tokenIn,
+            address payer,
+            bytes calldata swap
+        ) = data.decodeSingleCheckedArgs();
+        // We need to measure spent amount via balanceOf, as
+        // callbacks might execute additional swaps
+        uint256 balanceBefore = tokenIn.balanceOf(payer);
+        _singleSwap(givenAmount, swap);
+        calculatedAmount = balanceBefore - tokenIn.balanceOf(payer);
+        if (calculatedAmount > maxUserAmount) {
+            revert NegativeSlippage(calculatedAmount, maxUserAmount);
+        }
     }
 
+    /**
+     * @dev Executes a sequence of exact in swaps, checking that the user gets more
+     * than minUserAmount of buyToken.
+     */
     function sequentialExactIn(
         uint256 givenAmount,
         uint256 minUserAmount,
@@ -97,10 +138,30 @@ contract PropellerRouter is
         withSwapContext
         returns (uint256 calculatedAmount)
     {
-        calculatedAmount =
-            _sequentialExactIn(givenAmount, minUserAmount, swaps);
+        uint8 exchange;
+        bytes calldata swap;
+        calculatedAmount = givenAmount;
+
+        while (swaps.length > 0) {
+            (swap, swaps) = swaps.next();
+            exchange = swap.exchange();
+            calculatedAmount =
+                _executeSwap(exchange, calculatedAmount, swap.protocolData());
+        }
+        if (calculatedAmount < minUserAmount) {
+            revert NegativeSlippage(calculatedAmount, minUserAmount);
+        }
     }
 
+    /**
+     * @dev Executes a sequence of exact out swaps, by first quoting
+     *  backwards and then executing with corrected amounts a
+     *  sequential exactIn swap
+     *
+     * This method checks that the user spends no more than maxUserAmount of sellToken
+     *  Note: All used executors must implement ISwapQuoter, for this
+     *  method to work correctly.
+     */
     function sequentialExactOut(
         uint256 givenAmount,
         uint256 maxUserAmount,
@@ -112,12 +173,43 @@ contract PropellerRouter is
         withSwapContext
         returns (uint256 calculatedAmount)
     {
-        calculatedAmount =
-            _sequentialExactOut(givenAmount, maxUserAmount, swaps);
+        // Idea: On v2, reserve 14 bytes for calculatedAmount and replace them here
+        //  to save some quotes, if these 14 bytes are all zero the swap call won't
+        //  recalculate the quote else, it will simply execute with the calculatedAmount
+        //  that was passed along.
+        // TODO: Check if replacing 14 bytes (requires us to construct new swap in mempy)
+        //  amortises the repeated quoting (quoting again costs at least 500 gas, from
+        //  a quick calculation it should amortise)
+        bytes calldata swap;
+        uint256[] memory amounts = new uint256[](swaps.length + 1);
+        amounts[swaps.length] = givenAmount;
+
+        // backwards pass to get correct in amount
+        for (uint256 i = swaps.length; i > 0; i--) {
+            swap = swaps[i - 1];
+            amounts[i - 1] =
+                _quoteSwap(swap.exchange(), amounts[i], swap.protocolData());
+        }
+        for (uint8 i = 0; i < swaps.length; i++) {
+            swap = swaps[i];
+            _executeSwap(swap.exchange(), amounts[i + 1], swap.protocolData());
+        }
+        calculatedAmount = amounts[0];
+
+        if (calculatedAmount > maxUserAmount) {
+            revert NegativeSlippage(calculatedAmount, maxUserAmount);
+        }
     }
 
+    /**
+     * @dev Executes a swap graph with internal splits token amount
+     *  splits, checking that the user gets more than minUserAmount of buyToken.
+     *
+     *  Assumes the swaps in swaps_ already contain any required token
+     *  addresses.
+     */
     function splitExactIn(
-        uint256 amount,
+        uint256 amountIn,
         uint256 minUserAmount,
         SplitSwapExactInParameters calldata parameters
     )
@@ -127,9 +219,41 @@ contract PropellerRouter is
         withSwapContext
         returns (uint256 amountOut)
     {
-        amountOut = _splitExactIn(
-            amount, minUserAmount, parameters.nTokens, parameters.swaps
-        );
+        uint256 nTokens = parameters.nTokens;
+        bytes calldata swaps_ = parameters.swaps;
+
+        uint256 currentAmountIn;
+        uint256 currentAmountOut;
+        uint8 tokenInIndex;
+        uint8 tokenOutIndex;
+        uint24 split;
+        bytes calldata swap;
+
+        uint256[] memory remainingAmounts = new uint256[](nTokens);
+        uint256[] memory amounts = new uint256[](nTokens);
+        amounts[0] = amountIn;
+        remainingAmounts[0] = amountIn;
+
+        while (swaps_.length > 0) {
+            (swap, swaps_) = swaps_.next();
+            split = swap.splitPercentage();
+            tokenInIndex = swap.tokenInIndex();
+            tokenOutIndex = swap.tokenOutIndex();
+            currentAmountIn = split > 0
+                ? (amounts[tokenInIndex] * split) / 0xffffff
+                : remainingAmounts[tokenInIndex];
+            currentAmountOut = _executeSwap(
+                swap.exchange(), currentAmountIn, swap.protocolData()
+            );
+
+            amounts[tokenOutIndex] += currentAmountOut;
+            remainingAmounts[tokenOutIndex] += currentAmountOut;
+            remainingAmounts[tokenInIndex] -= currentAmountIn;
+        }
+        calculatedAmount = amounts[tokenOutIndex];
+        if (calculatedAmount < minUserAmount) {
+            revert NegativeSlippage(calculatedAmount, minUserAmount);
+        }
     }
 
     /**
@@ -239,4 +363,42 @@ contract PropellerRouter is
      * @dev Allows this contract to receive native token
      */
     receive() external payable {}
+
+
+    /**
+     * @dev Transfers ERC20 tokens or ETH out. Meant to transfer to the final receiver.
+     *
+     *  Note Can also transfer the complete balance but keeping 1
+     *  atomic unit dust for gas optimisation reasons. This is
+     *  automatically done if the transfer amount is 0.
+     */
+    function _transfer(uint256 amount, address receiver, IERC20 token)
+        internal
+    {
+        if (address(token) == address(0)) {
+            _transferNative(amount, payable(receiver));
+        } else {
+            if (amount == 0) {
+                token.transferBalanceLeavingDust(receiver);
+            } else {
+                token.safeTransfer(receiver, amount);
+            }
+        }
+    }
+
+    /**
+     * @dev Transfers ETH out. Meant to transfer to the final receiver.
+     */
+    function _transferNative(uint256 amount, address payable receiver)
+        internal
+    {
+        bool sent;
+        // ETH transfer via call see https://solidity-by-example.org/sending-ether/
+        assembly {
+            sent := call(gas(), receiver, amount, 0, 0, 0, 0)
+        }
+        if (!sent) {
+            revert InvalidTransfer(receiver, address(0), amount);
+        }
+    }
 }
