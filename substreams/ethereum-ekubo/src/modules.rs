65e0002f8c93a15033444d00c1eb0cff43ae1ced
kayibal <alan@datarevenue.com>
2025-02-01 19:38:04 -0300

    fix: Remove unnecessary store

--- substreams/ethereum-ekubo/src/modules.rs
+++ substreams/ethereum-ekubo/src/modules.rs
@@ -51,36 +51,11 @@ fn map_protocol_components(
     })
 }
 
-#[substreams::handlers::store]
-fn store_protocol_components(map_protocol_components: BlockTransactionProtocolComponents, store: StoreSetRaw) {
-    map_protocol_components.tx_components
-        .into_iter()
-        .for_each(|tx_pc| {
-            tx_pc
-                .components
-                .into_iter()
-                .for_each(|pc| {
-                    let tokens = serde_sibor::to_bytes(&pc.tokens).expect("Sibor encoding protocol component tokens failed");
-                    store.set(0, &pc.id, &tokens);
-                })
-        });
-}
-
 /// Extracts balance changes per component
 ///
-/// This template function inspects ERC20 transfer events to/from the singleton contract
-/// to extract balance changes.  If a transfer to the component is detected, it's
-/// balanced is increased and if a balance from the component is detected its balance
-/// is decreased.
-///
-/// ## Note:
-/// Changes are necessary if your protocol uses native ETH or your component burns or
-/// mints tokens without emitting transfer events.
-///
-/// You may want to ignore LP tokens if your protocol emits transfer events for these
-/// here.
+/// Indexes balance changes according to Ekubo core events.
 #[substreams::handlers::map]
-fn map_relative_component_balance(params: String, block: eth::v2::Block, store: StoreGetRaw) -> Result<BlockBalanceDeltas> {
+fn map_relative_component_balance(params: String, block: eth::v2::Block) -> Result<BlockBalanceDeltas> {
     let config: DeploymentConfig = serde_qs::from_str(params.as_str())?;
     let res = block
         .transactions()
@@ -92,92 +67,74 @@ fn map_relative_component_balance(params: String, block: eth::v2::Block, store: 
                     }
                     if let Some(ev) = core_events::PositionUpdated::match_and_decode(log) {
                         let pool_id = hash_pool_key(&ev.pool_key);
-                        // TODO better error handling
-                        let pool_tokens: Vec<Vec<u8>> = serde_sibor::from_bytes(
-                            &store.get_last(&pool_id).expect("Missing protocol component")
-                        ).expect("Decoding component tokens failed");
 
                         vec![
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[0].clone(),
+                                token: ev.pool_key.0.clone(),
                                 delta: ev.delta0.to_signed_bytes_be(),
                                 component_id: pool_id.clone().into(),
                             },
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[1].clone(),
+                                token: ev.pool_key.1.clone(),
                                 delta: ev.delta1.to_signed_bytes_be(),
                                 component_id: pool_id.into(),
                             }
                         ]
                     } else if let Some(ev) = core_events::PositionFeesCollected::match_and_decode(log) {
                         let pool_id = hash_pool_key(&ev.pool_key);
-                        // TODO better error handling
-                        let pool_tokens: Vec<Vec<u8>> = serde_sibor::from_bytes(
-                            &store.get_last(&pool_id).expect("Missing protocol component")
-                        ).expect("Decoding component tokens failed");
 
                         vec![
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[0].clone(),
+                                token: ev.pool_key.0.clone(),
                                 delta: ev.amount0.neg().to_signed_bytes_be(),
                                 component_id: pool_id.clone().into(),
                             },
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[1].clone(),
+                                token: ev.pool_key.1.clone(),
                                 delta: ev.amount1.neg().to_signed_bytes_be(),
                                 component_id: pool_id.into(),
                             }
                         ]
                     } else if let Some(ev) = core_events::Swapped::match_and_decode(log) {
                         let pool_id = hash_pool_key(&ev.pool_key);
-                        // TODO better error handling
-                        let pool_tokens: Vec<Vec<u8>> = serde_sibor::from_bytes(
-                            &store.get_last(&pool_id).expect("Missing protocol component")
-                        ).expect("Decoding component tokens failed");
-
                         vec![
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[0].clone(),
+                                token: ev.pool_key.0.clone(),
                                 delta: ev.delta0.to_signed_bytes_be(),
                                 component_id: pool_id.clone().into(),
                             },
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[1].clone(),
+                                token: ev.pool_key.1.clone(),
                                 delta: ev.delta1.to_signed_bytes_be(),
                                 component_id: pool_id.into(),
                             }
                         ]
                     } else if let Some(ev) = core_events::FeesAccumulated::match_and_decode(log) {
                         let pool_id = hash_pool_key(&ev.pool_key);
-                        // TODO better error handling
-                        let pool_tokens: Vec<Vec<u8>> = serde_sibor::from_bytes(
-                            &store.get_last(&pool_id).expect("Missing protocol component")
-                        ).expect("Decoding component tokens failed");
-
                         vec![
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[0].clone(),
+                                token: ev.pool_key.0.clone(),
                                 delta: ev.amount0.to_signed_bytes_be(),
                                 component_id: pool_id.clone().into(),
                             },
                             BalanceDelta {
                                 ord: log.ordinal,
                                 tx: Some(tx.into()),
-                                token: pool_tokens[1].clone(),
+                                token: ev.pool_key.1.clone(),
                                 delta: ev.amount1.to_signed_bytes_be(),
                                 component_id: pool_id.into(),
                             }
@@ -197,9 +154,6 @@ fn map_relative_component_balance(params: String, block: eth::v2::Block, store: 
 ///
 /// Aggregate the relative balances in an additive store since tycho-indexer expects
 /// absolute balance inputs.
-///
-/// ## Note:
-/// This method should usually not require any changes.
 #[substreams::handlers::store]
 pub fn store_component_balances(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
     tycho_substreams::balances::store_balance_changes(deltas, store);
@@ -209,10 +163,6 @@ pub fn store_component_balances(deltas: BlockBalanceDeltas, store: StoreAddBigIn
 ///
 /// This is the main method that will aggregate all changes as well as extract all
 /// relevant contract storage deltas.
-///
-/// ## Note:
-/// You may have to change this method if your components have any default dynamic
-/// attributes, or if you need any additional static contracts indexed.
 #[substreams::handlers::map]
 fn map_protocol_changes(
     params: String,
