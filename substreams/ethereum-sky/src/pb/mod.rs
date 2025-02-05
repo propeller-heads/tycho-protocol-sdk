9f45f85089fdedda94fbe89c87dec15788eec31b
Mattia <mp.web3@gmail.com>
2025-02-05 11:54:58 +0100

    first build

--- substreams/ethereum-sky/src/pb/mod.rs
+++ substreams/ethereum-sky/src/pb/mod.rs
@@ -1,3 +1,11 @@
+// @generated
+pub mod contract {
+    // @@protoc_insertion_point(attribute:contract.v1)
+    pub mod v1 {
+        include!("contract.v1.rs");
+        // @@protoc_insertion_point(contract.v1)
+    }
+}
 pub mod tycho {
     pub mod evm {
         // @@protoc_insertion_point(attribute:tycho.evm.v1)
