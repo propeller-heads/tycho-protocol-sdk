9b328272da92b12677d8e0d8e75c1763491f692e
Louise Poole <louise@datarevenue.com>
2025-02-26 17:52:47 +0200

    fix: fix double referencing

--- substreams/ethereum-uniswap-v4/src/modules/3_map_events.rs
+++ substreams/ethereum-uniswap-v4/src/modules/3_map_events.rs
@@ -31,7 +31,7 @@ pub fn map_events(
             receipt
                 .logs
                 .iter()
-                .filter_map(|log| log_to_event(&log, &tx, &pools_store))
+                .filter_map(|log| log_to_event(log, &tx, &pools_store))
                 .collect::<Vec<_>>()
         })
         .collect::<Vec<_>>();
