a0566d3504dfd29d977212d2cdb4abb0a8092a8e
Samarendra Gouda <samarendragouda@gmail.com>
2025-03-12 14:55:59 +0530

    fix(solidity): executor contract

--- evm/src/fluid/FluidSwapExecutor.sol
+++ evm/src/fluid/FluidSwapExecutor.sol
@@ -2,55 +2,51 @@
 pragma solidity ^0.8.0;
 
 import "../interfaces/ISwapExecutor.sol";
-import {FluidDexReservesResolver} from "./Interfaces/FluidInterfaces.sol";
 import {IFluidDexT1} from "./Interfaces/iDexT1.sol";
 
-
 contract FluidSwapExecutor is ISwapExecutor {
-
-    /// @notice Reserves resolver contract
-    FluidDexReservesResolver public immutable resolver;
-
-    /// @notice Constructor to set the reserves resolver
-    /// @param resolver_ Address of the reserves resolver contract
-    constructor(address resolver_) {
-        resolver = FluidDexReservesResolver(resolver_);
-    }
-
     /**
-     * @dev Executes a Balancer swap.
-     * @param givenAmount how much of to swap, depending on exactOut either in-
-     * or outAmount.
+     * @dev Executes a Fluid DEX swap.
+     * @param givenAmount how much of to swap, depending on exactOut either in or out Amount.
      * @param data the parameters of the swap. This data is roughly the packed
      * encoding of
-     *      poolId
+     *      poolAddress
      *      sellToken
      *      buyToken
      *      side    // true for sell and false for buy
      */
-    function swap(uint256 givenAmount, bytes calldata data)
-        external
-        payable
-        returns (uint256 calculatedAmount)
-    {
-
-    (bytes32 poolId, address sellToken, address buyToken, bool side) = abi.decode(data, (bytes32, address, address, bool));
+    function swap(
+        uint256 givenAmount,
+        bytes calldata data
+    ) external payable returns (uint256 calculatedAmount) {
+        (
+            address poolAddress,
+            address sellToken,
+            address buyToken,
+            bool side
+        ) = abi.decode(data, (address, address, address, bool));
         if (givenAmount == 0) {
             return 0;
         }
-        address poolAddress = resolver.getPoolAddress(uint256(poolId));
+
         IFluidDexT1 pool = IFluidDexT1(poolAddress);
+        address token0 = pool.constantsView().token0;
 
-        (address token0,) = resolver.getPoolTokens(poolAddress);
-
-        if(sellToken != 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE){
-            IERC20(sellToken).transferFrom(msg.sender, address(this), givenAmount);
+        if (sellToken != 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
+            IERC20(sellToken).transferFrom(
+                msg.sender,
+                address(this),
+                givenAmount
+            );
             IERC20(sellToken).approve(poolAddress, givenAmount);
         }
 
         if (side) {
             calculatedAmount = pool.swapIn{value: msg.value}(
-                sellToken == token0, givenAmount, 0, msg.sender
+                sellToken == token0,
+                givenAmount,
+                0,
+                msg.sender
             );
         } else {
             calculatedAmount = pool.swapOut{value: msg.value}(
@@ -62,5 +58,5 @@ contract FluidSwapExecutor is ISwapExecutor {
         }
     }
 
-    function receive() external payable {}
+    receive() external payable {}
 }
