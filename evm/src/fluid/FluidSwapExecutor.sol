c02eab6df5087e1c79d1250c06edb4f244be1264
Samarendra Gouda <samarendragouda@gmail.com>
2025-03-13 23:39:15 +0530

    fix: upgrade solc and use transient storage

--- evm/src/fluid/FluidSwapExecutor.sol
+++ evm/src/fluid/FluidSwapExecutor.sol
@@ -1,19 +1,19 @@
-// SPDX-License-Identifier: UNLICENCED
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
 import "../interfaces/ISwapExecutor.sol";
 import {IFluidDexT1} from "./Interfaces/iDexT1.sol";
 import {IDexCallback} from "./Interfaces/DexCallback.sol";
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract FluidSwapExecutor is ISwapExecutor, IDexCallback {
     using SafeERC20 for IERC20;
-    address constant DEAD_ADDRESS = 0x000000000000000000000000000000000000dEaD;
     address public immutable FLUID_LIQUIDITY;
-    address public senderTransient;
+    address transient sender;
+    address transient dexAddress; 
 
     constructor(address fluidLiquidity_) {
         FLUID_LIQUIDITY = fluidLiquidity_;
-        senderTransient = DEAD_ADDRESS;
     }
 
 
@@ -33,39 +33,11 @@ contract FluidSwapExecutor is ISwapExecutor, IDexCallback {
         (bool swap0to1, address poolAddress, address sellToken) 
             = abi.decode(data, (bool, address, address));
 
-        senderTransient = msg.sender;
-        assembly {
-            if iszero(givenAmount) {
-                revert(0, 0x20)
-            }
+        sender = msg.sender;
+        dexAddress = poolAddress;
 
-            let nativeToken := 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
-            if iszero(eq(sellToken, nativeToken)) {
-                let transferFromCallData := mload(0x40)
-                mstore(transferFromCallData, 0x23b872dd) // Function selector for transferFrom(address,address,uint256)
-
-                mstore(add(transferFromCallData, 0x04), caller()) // msg.sender
-                mstore(add(transferFromCallData, 0x24), address()) // address(this)
-                mstore(add(transferFromCallData, 0x44), givenAmount) // givenAmount
-
-                let success := call(
-                    gas(),
-                    IERC20(sellToken),
-                    0,
-                    transferFromCallData,
-                    0x64,
-                    0,
-                    0
-                )
-
-                if iszero(success) {
-                    returndatacopy(0, 0, returndatasize())
-                    revert(0, returndatasize())
-                }
-            }
-        }
-
-        IFluidDexT1 pool = IFluidDexT1(poolAddress);
+        IERC20(sellToken).safeTransferFrom(sender, address(this), givenAmount);
+        IFluidDexT1 pool = IFluidDexT1(dexAddress);
 
         if (swap0to1) {
             calculatedAmount = pool.swapInWithCallback{value: msg.value}(
@@ -86,34 +58,8 @@ contract FluidSwapExecutor is ISwapExecutor, IDexCallback {
 
     /// @notice Callback function called by the DEX
     function dexCallback(address token_, uint256 amount_) external override {
-        require(msg.sender == FLUID_DEX, "Invalid caller");
-
-        // Transfer tokens from the senderTransient to Fluid liquidity
-        assembly {
-            let transferFromCallData := mload(0x40)
-                mstore(transferFromCallData, 0x23b872dd) // Function selector for transferFrom(address,address,uint256)
-
-                mstore(add(transferFromCallData, 0x04), senderTransient)
-                mstore(add(transferFromCallData, 0x24), FLUID_LIQUIDITY)
-                mstore(add(transferFromCallData, 0x44), amount_)
-
-                let success := call(
-                    gas(),
-                    IERC20(token_),
-                    0,
-                    transferFromCallData,
-                    0x64,
-                    0,
-                    0
-                )
-
-                if iszero(success) {
-                    returndatacopy(0, 0, returndatasize())
-                    revert(0, returndatasize())
-                }
-        }
-
-        senderTransient = DEAD_ADDRESS; // Reset storage for gas refunds
+        require(msg.sender == dexAddress, "Invalid caller");
+        IERC20(token_).safeTransferFrom(sender, FLUID_LIQUIDITY, amount_);
     }
 
     receive() external payable {}
