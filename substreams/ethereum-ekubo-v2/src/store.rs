2ab6a648b284fc694655209c2c83bd9251eb4f52
die-herdplatte <173669014+die-herdplatte@users.noreply.github.com>
2025-04-12 11:30:03 +0200

    Remove unnecessary parameterization

--- substreams/ethereum-ekubo-v2/src/store.rs
+++ substreams/ethereum-ekubo-v2/src/store.rs
@@ -6,7 +6,7 @@ pub fn store_method_from_change_type<T, S: StoreSetSum<T>>(
     change_type: ChangeType,
 ) -> fn(&S, u64, String, T) {
     match change_type {
-        ChangeType::Delta => StoreSetSum::sum::<String>,
+        ChangeType::Delta => StoreSetSum::sum,
         ChangeType::Absolute => StoreSetSum::set,
     }
 }
