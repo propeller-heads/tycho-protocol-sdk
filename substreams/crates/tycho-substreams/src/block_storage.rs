b54b984e5d22d80544ab8862236c0169de7bbcd0
Diana Carvalho <diana@propellerheads.xyz>
2025-05-13 10:59:52 +0100

    fix: Filter out calls that are reverted and sort change by ordinal
    #time 0m

--- substreams/crates/tycho-substreams/src/block_storage.rs
+++ substreams/crates/tycho-substreams/src/block_storage.rs
@@ -9,10 +9,10 @@ use crate::{
 
 #[allow(dead_code)]
 /// Helper function to extract all storage changes on a block.
-/// The raw block information collected is intended to be used by the DCI (Dynamic Contract Indexer) to extract 
-/// and index relevant changes. This is specifically for dynamically identified contracts that the DCI has chosen to 
-/// index. Note that core protocol data should still be properly integrated and indexed by the
-/// substreams package as per usual.
+/// The raw block information collected is intended to be used by the DCI (Dynamic Contract Indexer)
+/// to extract and index relevant changes. This is specifically for dynamically identified contracts
+/// that the DCI has chosen to index. Note that core protocol data should still be properly
+/// integrated and indexed by the substreams package as per usual.
 ///
 /// ## Panics
 /// Panics if the provided block is not an extended block model, as indicated by its detail level.
@@ -29,27 +29,50 @@ fn get_block_storage_changes(block: &eth::v2::Block) -> Vec<TransactionStorageCh
     for block_tx in block.transactions() {
         let transaction: Transaction = block_tx.into();
 
-        let mut storage_changes_per_address: HashMap<Vec<u8>, Vec<ContractSlot>> = HashMap::new();
+        let mut storage_changes = vec![];
         for call in block_tx.calls.iter() {
-            for storage_change in call.storage_changes.iter() {
-                let contract_slot = ContractSlot {
-                    slot: storage_change.clone().key,
-                    value: storage_change.clone().new_value,
-                };
-                storage_changes_per_address
-                    .entry(storage_change.clone().address)
-                    .or_default()
-                    .push(contract_slot);
+            // Filter out calls that are reverted
+            if !call.state_reverted {
+                for storage_change in call.storage_changes.iter() {
+                    storage_changes.push(storage_change.clone());
+                }
             }
         }
 
-        let storage_changes = storage_changes_per_address
+        // This is needed to get only the latest storage change for each address and slot
+        storage_changes.sort_unstable_by_key(|change| change.ordinal);
+        let mut storage_changes_per_address_and_slot: HashMap<(Vec<u8>, Vec<u8>), ContractSlot> =
+            HashMap::new();
+        for storage_change in storage_changes.iter() {
+            let contract_slot = ContractSlot {
+                slot: storage_change.clone().key,
+                value: storage_change.clone().new_value,
+            };
+            storage_changes_per_address_and_slot.insert(
+                (storage_change.address.clone(), storage_change.key.clone()),
+                contract_slot,
+            );
+        }
+
+        let tx_storage_changes: Vec<StorageChanges> = storage_changes_per_address_and_slot
+            .into_iter()
+            .fold(
+                HashMap::new(),
+                |mut acc: HashMap<Vec<u8>, Vec<ContractSlot>>, ((address, _key), slot)| {
+                    acc.entry(address)
+                        .or_default()
+                        .push(slot);
+                    acc
+                },
+            )
             .into_iter()
             .map(|(address, slots)| StorageChanges { address, slots })
-            .collect::<Vec<_>>();
+            .collect();
 
-        block_storage_changes
-            .push(TransactionStorageChanges { tx: Some(transaction), storage_changes });
+        block_storage_changes.push(TransactionStorageChanges {
+            tx: Some(transaction),
+            storage_changes: tx_storage_changes,
+        });
     }
 
     block_storage_changes
