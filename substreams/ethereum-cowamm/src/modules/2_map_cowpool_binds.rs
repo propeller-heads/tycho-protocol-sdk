f75981b097635d10e8536613e26d1e34ba1a1826
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-05-29 14:18:45 +0100

    feat:removed errors, saved tx protobuf passing

--- substreams/ethereum-cowamm/src/modules/2_map_cowpool_binds.rs
+++ substreams/ethereum-cowamm/src/modules/2_map_cowpool_binds.rs
@@ -1,4 +1,4 @@
-use crate::pb::cowamm::{CowPoolCreations,CowpoolBind, CowPoolBinds};
+use crate::pb::cowamm::{CowPoolCreations,CowPoolBind, CowPoolBinds};
 use anyhow::{Ok, Result};
 use ethabi::ethereum_types::Address;
 use substreams::{prelude::BigInt};
@@ -8,33 +8,35 @@ use substreams_ethereum::pb::eth::v2::{Call,Block, Log,};
 use tycho_substreams::prelude::*;
 
 #[substreams::handlers::map]
-pub fn map_cowpool_binds(block: Block, creations: CowpoolCreations) -> Vec<CowpoolBind> {
+pub fn map_cowpool_binds(block: Block, creations: CowPoolCreations) -> Result<CowPoolBinds> {
     const BIND_TOPIC: &str = "0xe4e1e53800000000000000000000000000000000000000000000000000000000";
 
-    let creation_addrs: std::collections::HashSet<&Vec<u8>> = creations.creations.iter().map(|c| &c.address).collect();
+    let creation_addrs: std::collections::HashSet<&Vec<u8>> = creations.pools.iter().map(|c| &c.address).collect();
 
     let cowpool_binds = block
         .logs()
         .filter(|log| {
-            log.topics.get(0).map(|t| hex::encode(t)) == Some(BIND_TOPIC) &&
-            creation_addrs.contains(&log.address)
+            log.topics()
+            .get(0)
+            .map(|t| hex::encode(t)).as_ref() == Some(&BIND_TOPIC.to_string()) &&
+            creation_addrs.contains(&log.address().to_vec())
         })
         .filter_map(|log| {
-            let data = &log.data;
+            let data = &log.data();
             if data.len() < 165 { return None; } // is this necessary?
             let token = data.get(80..100)?.to_vec();
             let weight_bytes = data.get(132..164)?;
-            let weight = substreams::scalar::BigInt::from_bytes_be(weight_bytes).to_u64(); //change this to an expect?
+            let weight = substreams::scalar::BigInt::from_signed_bytes_be(weight_bytes).to_u64(); //change this to an expect? //
  
             Some(
                CowPoolBind {
-                address: log.address.clone(),
+                address: log.address().to_vec(),
                 token,
-                weight: weight.to_u64().unwrap_or(0), //is this necessary? or it should just panic
+                weight: weight, 
             })
         })
-        .collect<Vec<CowPoolBind>>();
+        .collect::<Vec<CowPoolBind>>();
 
-     Ok(CowPoolBinds { cowpool_binds })
+     Ok(CowPoolBinds { binds: cowpool_binds })
 }
 
