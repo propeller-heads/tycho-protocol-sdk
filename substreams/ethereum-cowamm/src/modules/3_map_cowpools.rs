3fbab00b9036084b2d7f633a844de38f4e910101
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-05-29 14:18:45 +0100

    feat:removed errors, saved tx protobuf passing

--- substreams/ethereum-cowamm/src/modules/3_map_cowpools.rs
+++ substreams/ethereum-cowamm/src/modules/3_map_cowpools.rs
@@ -1,24 +1,22 @@
 use std::collections::HashMap;
 use crate::{modules::utils::Params};
-use crate::pb::cowamm::{CowPool, CowPools, CowpoolBind, CowPoolBinds, CowPoolCreation, CowPoolCreations};
+use crate::pb::cowamm::{CowPool, CowPools, CowPoolBind, CowPoolBinds, CowPoolCreation, CowPoolCreations};
 use anyhow::{Ok, Result};
 use ethabi::ethereum_types::Address;
 use substreams::{prelude::BigInt};
 use hex_literal::hex;
 use serde::Deserialize;
-use substreams_ethereum::pb::eth::v2::{Block};
-use substreams_ethereum::Event;
 use tycho_substreams::prelude::*;
 
 #[substreams::handlers::map]
-pub fn map_cowpools(creations: CowPoolCreations, binds: CowPoolBinds>) -> Result<CowPool, substreams::errors::Error> {
+pub fn map_cowpools(creations: CowPoolCreations, binds: CowPoolBinds) -> Result<CowPools, substreams::errors::Error> {
     
     //combine cow_pool_binds and cow_pool_creations based on the address of the pool
     // like an left join in SQL 
 
-    //hashmap of pool addresses with token and weight
+    //hashmap of pool addresses with token and its weight
     //should we turn the vec to a hex address? i think that'll be better
-    let mut binds_by_address: HashMap<Vec<u8>, Vec<Vec<u8>, u64>> = HashMap::new();
+    let mut binds_by_address: HashMap<Vec<u8>, Vec<(Vec<u8>, u64)>> = HashMap::new();
 
     for bind in binds.binds {
         binds_by_address
@@ -26,14 +24,24 @@ pub fn map_cowpools(creations: CowPoolCreations, binds: CowPoolBinds>) -> Result
             .or_default()
             .push((bind.token, bind.weight));  
             // in this hashmap if we have more than one element with the same key, it gets pushed to it 
-            // so we'll have Vec<Vec<Vec<u8>, u64>, Vec<Vec<u8>, u64>> for a pool with two tokens 
+            // so we'll have Vec<Vec<u8> Vec<(Vec<u8>, u64)>>> for a pool with two tokens 
+    }
+
+    // map the tx_hash to its TransactionTrace
+    let mut txs_by_hash = HashMap<Vec<u8>, Transaction> = HashMap::new();
+    for creation in creation.pools {
+        txs_by_hash
+            .entry(creation.created_tx_hash.clone())
+            .or_default()
+            .push(creation.tx);  
     }
     // Create pools by joining on address
     let mut pools: Vec<CowPool> = Vec::new();
+
     for creation in creations.pools {
         if let Some(binds) = binds_by_address.get(&creation.address) {
-            if tokens.len() != 2 {
-                continue; // skip incomplete pools
+            if binds.len() != 2 {
+                continue; // skip incomplete pools if any 
             }
 
             let (token_a, weight_a, token_b, weight_b) = {
@@ -52,12 +60,14 @@ pub fn map_cowpools(creations: CowPoolCreations, binds: CowPoolBinds>) -> Result
                 lp_token: creation.lp_token.clone(),
                 weight_a,
                 weight_b,
-                fee: 0, 
+                fee: 0,
+                // tx: creation.tx, // why is it an Option?
                 created_tx_hash: creation.created_tx_hash.clone(),
             });
+
         }
     }
 
-    Ok(CowPools { pools })
+    Ok(CowPoolsWithTx { pools , txs})
 }
 
--- substreams/ethereum-cowamm/src/modules/4_map_components.rs
+++ substreams/ethereum-cowamm/src/modules/4_map_components.rs
@@ -1,25 +1,28 @@
-use std::collections::HashMap;
-use std::str::FromStr;
 use crate::{modules::utils::Params};
-use crate::pb::cowamm::{CowPool, CowPools, CowpoolBind, CowPoolBinds, CowPoolCreation, CowPoolCreations};
 use anyhow::{Ok, Result};
 use ethabi::ethereum_types::Address;
 use substreams::{prelude::BigInt};
 use hex_literal::hex;
 use serde::Deserialize;
+use crate::pb::cowamm::{CowPool, CowPools};
 use substreams_ethereum::pb::eth::v2::{Call,Block, Log, TransactionTrace};
 use substreams_ethereum::Event;
 use tycho_substreams::prelude::*;
 use crate::abi::{b_cow_factory::events::CowammPoolCreated, b_cow_pool::functions::Bind};
 use substreams_helper::{event_handler::EventHandler, hex::Hexable};
 
+fn getTransactionTrace(block: Block, pool:CowPool) {
+   return block
+            .transactions()
+            .find(|txn| {                   
+                pool.created_tx_hash == txn.hash;
+            })
+}
 
-fn build_component(
-    pool:CowPool
+fn create_component(
+    factory_address: &[u8],
+    pool: CowPool,
 ) -> Option<ProtocolComponent> {
-            
-    let params = Params::parse_from_query(&params)?;
-    let factory_address = params.decode_addresses().unwrap();
 
     substreams::log::info!(
         "CowammPoolCreated Event: pool address = {}",
@@ -27,16 +30,16 @@ fn build_component(
     );
 
     Some(
-        ProtocolComponent::new(&pool.to_hex(), &(tx.into()))
-        .with_tokens(&[pool.token_a.as_slice(), pool.token_a.as_slice(), pool.lp_token])  //remember the address of the contract is the address of the lp token  
+        ProtocolComponent::new(&pool.address.to_hex()) // &(tx.into()
+        .with_tokens(&[pool.token_a.as_slice(), pool.token_a.as_slice(), pool.lp_token.as_slice()])  //remember the address of the contract is the address of the lp token  
         .with_contracts(&[
             factory_address,
             pool.address.as_slice()
         ])
         .with_attributes(&[  
             ("fee", BigInt::from(0).to_signed_bytes_be()), // not 99.999% ? cuz thats what we are calculating it as 
-            ("denormalized_weight_a", pool.weight_a.to_signed_bytes_be()), 
-            ("denormalized_weight_b", pool.weight_b.to_signed_bytes_be()),
+            ("denormalized_weight_a", BigInt::from(pool.weight_a).to_signed_bytes_be()), 
+            ("denormalized_weight_b", BigInt::from(pool.weight_b).to_signed_bytes_be()),
         ])
         .as_swap_type("cowamm_pool", ImplementationType::Vm)
     )
@@ -44,29 +47,29 @@ fn build_component(
 }
 
 
-pub fn map_component(params: String, CowPools: pools) -> Result<> {
+pub fn map_components(params: String, pools: CowPools) -> Result<BlockTransactionProtocolComponents> {
     substreams::log::info!("--- map_components called ---");   
-    substreams::log::info!("Received params: {}", params);
-    substreams::log::info!("Block number: {}", block.number);
+    // substreams::log::info!("Received params: {}", params);
+    // substreams::log::info!("Block number: {}", block.number);
+    let params = Params::parse_from_query(&params)?;
+    let factory_address = params.decode_addresses().unwrap();
 
         Ok(BlockTransactionProtocolComponents {
             tx_components: 
             pools
+                .pools
                 .iter()
-                .filter_map(|pool|
-                    let components = pools
-                        .iter()
-                        .map(|pool| {
-                            build_component(pool)
-                        })
+                .filter_map(|pool| {
+                   
+                    let components = create_component(&factory_address, pool.clone())
+                        .into_iter()
                         .collect::<Vec<_>>();
-            
-                    if !components.is_empty() {
-                        Some(TransactionProtocolComponents { tx: Some(pool.created_tx_hash), components }) 
+                    if !components.is_empty() { // pool.tx is Option<cowamm::Transaction>
+                        Some(TransactionProtocolComponents { tx: pool.tx.into(), components }) 
                     } else {
                         None
                     }            
-                )
+                })
                 .collect::<Vec<_>>(),
     })
 }
