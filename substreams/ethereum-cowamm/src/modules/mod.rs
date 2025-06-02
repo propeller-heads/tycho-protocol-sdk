b1b3c0eb4e1df71f961c140c810dae17236c8dae
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-06-02 13:22:48 +0100

    feat:cowpool creations to map_component works

--- substreams/ethereum-cowamm/src/modules/mod.rs
+++ substreams/ethereum-cowamm/src/modules/mod.rs
@@ -1,34 +1,46 @@
 pub use map_cowpool_creations::map_cowpool_creations;
+pub use store_cowpool_creations::store_cowpool_creations;
 pub use map_cowpool_binds::map_cowpool_binds;
+pub use store_cowpool_binds::store_cowpool_binds;
 pub use map_cowpools::map_cowpools;
+pub use store_cowpools::store_cowpools;
 pub use map_components::map_components;
 pub use store_components::store_components;
-pub use map_protocol_changes::map_protocol_changes;
 pub use map_relative_balances::map_relative_balances;
 pub use store_balances::store_balances;
+pub use map_protocol_changes::map_protocol_changes;
 
 
 #[path = "1_map_cowpool_creations.rs"]
 mod map_cowpool_creations;
 
+#[path = "1_store_cowpool_creations.rs"]
+mod store_cowpool_creations;
+
 #[path = "2_map_cowpool_binds.rs"]
 mod map_cowpool_binds;
+
+#[path = "2_store_cowpool_binds.rs"]
+mod store_cowpool_binds;
 
 #[path = "3_map_cowpools.rs"]
 mod map_cowpools;
 
+#[path = "3_store_cowpools.rs"]
+mod store_cowpools;
+
 #[path = "4_map_components.rs"]
 mod map_components;
 
-#[path = "5_store_components.rs"]
+#[path = "4_store_components.rs"]
 mod store_components;
 
-#[path = "6_map_relative_balances.rs"]
+#[path = "5_map_relative_balances.rs"]
 mod map_relative_balances;
 
-#[path = "7_store_balances.rs"]
+#[path = "5_store_balances.rs"]
 mod store_balances;
 
-#[path = "8_map_protocol_changes.rs"]
+#[path = "6_map_protocol_changes.rs"]
 mod map_protocol_changes;
 mod utils;
