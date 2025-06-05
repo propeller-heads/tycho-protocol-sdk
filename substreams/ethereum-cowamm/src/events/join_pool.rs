48c0af3526160237c0e5f9d0122cee3caee26de3
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-06-05 05:00:45 +0100

    feat:added events folder, remmoved unnecessary protobuf bindings

---
+++ substreams/ethereum-cowamm/src/events/join_pool.rs
@@ -0,0 +1,28 @@
+use crate::{
+    abi::b_cow_pool::events::LogJoin, events::BalanceEventTrait, pb::cowamm::CowPool,
+};
+use substreams_helper::hex::Hexable;
+use tycho_substreams::prelude::*;
+
+impl BalanceEventTrait for LogJoin {
+    fn get_balance_delta(&self, tx: &Transaction, pool: &CowPool, ordinal: u64) -> Vec<BalanceDelta> {
+        let changed_balance = vec![
+            BalanceDelta {
+                ord: ordinal,
+                tx: Some(tx.clone()),
+                token: self.token_in.clone(),
+                delta: self
+                    .token_amount_in
+                    .clone()
+                    .to_signed_bytes_be(),
+                component_id: pool
+                    .address
+                    .clone()
+                    .to_hex()
+                    .as_bytes()
+                    .to_vec(),
+            }
+        ];
+        changed_balance
+    }
+}
