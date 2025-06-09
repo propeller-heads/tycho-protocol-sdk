b3402e23b9ba41ad9bddd30e40c3fc7ff343c2d9
Louise Poole <louise@datarevenue.com>
2025-06-09 17:32:03 +0200

    feat: add rate_providers static attributes

--- substreams/ethereum-balancer-v3/src/pool_factories.rs
+++ substreams/ethereum-balancer-v3/src/pool_factories.rs
@@ -16,14 +16,20 @@ use substreams_ethereum::{
     Event, Function,
 };
 use tycho_substreams::{
-    abi::erc20::events::Transfer, attributes::json_serialize_bigint_list, prelude::*,
+    abi::erc20::events::Transfer,
+    attributes::{json_serialize_address_list, json_serialize_bigint_list},
+    prelude::*,
 };
 
+// Token config: (token_address, rate, rate_provider_address, is_exempt_from_yield_fees)
 type TokenConfig = Vec<(Vec<u8>, substreams::scalar::BigInt, Vec<u8>, bool)>;
-pub fn check_erc4626(tokens: &TokenConfig) -> bool {
+
+pub fn collect_rate_providers(tokens: &TokenConfig) -> Vec<Vec<u8>> {
     tokens
         .iter()
-        .any(|token| token.1 == BigInt::from(1)) // WITH_RATE == 1
+        .filter(|token| token.1 == BigInt::from(1)) // WITH_RATE == 1
+        .map(|token| token.2.clone())
+        .collect::<Vec<_>>()
 }
 
 pub fn address_map(
@@ -40,26 +46,34 @@ pub fn address_map(
                 ..
             } = WeightedPoolCreate::match_and_decode(call)?;
             let WeightedPoolCreated { pool } = WeightedPoolCreated::match_and_decode(log)?;
-            let is_erc4626 = check_erc4626(&token_config);
+            let rate_providers = collect_rate_providers(&token_config);
+
             let tokens = token_config
                 .into_iter()
                 .map(|t| t.0)
                 .collect::<Vec<_>>();
 
+            let normalized_weights_bytes =
+                json_serialize_bigint_list(normalized_weights.as_slice());
+            let fee_bytes = swap_fee_percentage.to_signed_bytes_be();
+            let rate_providers_bytes = json_serialize_address_list(rate_providers.as_slice());
+
+            let mut attributes = vec![
+                ("pool_type", "WeightedPoolFactory".as_bytes()),
+                ("normalized_weights", &normalized_weights_bytes),
+                ("fee", &fee_bytes),
+                ("manual_updates", &[1u8]),
+            ];
+
+            if !rate_providers.is_empty() {
+                attributes.push(("rate_providers", &rate_providers_bytes));
+            }
+
             Some(
                 ProtocolComponent::new(&format!("0x{}", hex::encode(&pool)))
                     .with_contracts(&[pool, VAULT_ADDRESS.to_vec()])
                     .with_tokens(tokens.as_slice())
-                    .with_attributes(&[
-                        ("pool_type", "WeightedPoolFactory".as_bytes()),
-                        (
-                            "normalized_weights",
-                            &json_serialize_bigint_list(normalized_weights.as_slice()),
-                        ),
-                        ("fee", &swap_fee_percentage.to_signed_bytes_be()),
-                        ("manual_updates", &[1u8]),
-                        ("erc4626", &[is_erc4626 as u8]),
-                    ])
+                    .with_attributes(&attributes)
                     .as_swap_type("balancer_v3_pool", ImplementationType::Vm),
             )
         }
@@ -67,22 +81,31 @@ pub fn address_map(
             let StablePoolCreate { tokens: token_config, swap_fee_percentage, .. } =
                 StablePoolCreate::match_and_decode(call)?;
             let StablePoolCreated { pool } = StablePoolCreated::match_and_decode(log)?;
-            let is_erc4626 = check_erc4626(&token_config);
+            let rate_providers = collect_rate_providers(&token_config);
             let tokens = token_config
                 .into_iter()
                 .map(|t| t.0)
                 .collect::<Vec<_>>();
+
+            let fee_bytes = swap_fee_percentage.to_signed_bytes_be();
+            let rate_providers_bytes = json_serialize_address_list(rate_providers.as_slice());
+
+            let mut attributes = vec![
+                ("pool_type", "StablePoolFactory".as_bytes()),
+                ("bpt", &pool),
+                ("fee", &fee_bytes),
+                ("manual_updates", &[1u8]),
+            ];
+
+            if !rate_providers.is_empty() {
+                attributes.push(("rate_providers", &rate_providers_bytes));
+            }
+
             Some(
                 ProtocolComponent::new(&format!("0x{}", hex::encode(&pool)))
                     .with_contracts(&[pool.to_owned(), VAULT_ADDRESS.to_vec()])
                     .with_tokens(tokens.as_slice())
-                    .with_attributes(&[
-                        ("pool_type", "StablePoolFactory".as_bytes()),
-                        ("bpt", &pool),
-                        ("fee", &swap_fee_percentage.to_signed_bytes_be()),
-                        ("manual_updates", &[1u8]),
-                        ("erc4626", &[is_erc4626 as u8]),
-                    ])
+                    .with_attributes(&attributes)
                     .as_swap_type("balancer_v3_pool", ImplementationType::Vm),
             )
         }
