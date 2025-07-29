20311506ec5872017f12ea155d9bfee56fb137fb
adpthegreat <adpthegreatanddefiant@gmail.com>
2025-07-29 17:41:32 +0100

    chore:ran cargo fmt

--- substreams/ethereum-cowamm/src/events/transfer.rs
+++ substreams/ethereum-cowamm/src/events/transfer.rs
@@ -1,31 +1,33 @@
-use crate::{
-    abi::b_cow_pool::events::Transfer, events::BalanceEventTrait, pb::cowamm::CowPool,
-};
+use crate::{abi::b_cow_pool::events::Transfer, events::BalanceEventTrait, pb::cowamm::CowPool};
 use substreams_ethereum::{pb::eth::v2::Log, Event};
 use substreams_helper::hex::Hexable;
 use tycho_substreams::prelude::*;
 
 impl BalanceEventTrait for Transfer {
-    fn get_balance_delta(&self, tx: &Transaction, pool: &CowPool, event: &Log) -> Vec<BalanceDelta> {
+    fn get_balance_delta(
+        &self,
+        tx: &Transaction,
+        pool: &CowPool,
+        event: &Log,
+    ) -> Vec<BalanceDelta> {
         let mut changed_balances: Vec<BalanceDelta> = vec![];
-         let pool_addr = &pool.address;
-        const NULL_ADDRESS: [u8; 20] = [0u8; 20]; 
-        //what causes the lp token supply to change is either -> minting or burning 
-         //topics[1]: from address (padded to 32 bytes)
-         //topics[2]: to address (padded to 32 bytes)
+        const NULL_ADDRESS: [u8; 20] = [0u8; 20];
+        //what causes the lp token supply to change is either -> minting or burning
+        //topics[1]: from address (padded to 32 bytes)
+        //topics[2]: to address (padded to 32 bytes)
 
-        // when a user redeems tokens, they send the tokens to the null address, effectively burning them and thats a negative delta 
+        // when a user redeems tokens, they send the tokens to the null address, effectively burning them and thats a negative delta
         // https://etherscan.io/tx/0xc139e807a155b0ca1fdd5e870350fd623801671289982ab6300007cc7556c5a8#eventlog#250
         if event.address == pool.address && event.topics.get(1).unwrap()[12..] == NULL_ADDRESS {
-
             changed_balances.push(BalanceDelta {
-                ord:event.ordinal,
+                ord: event.ordinal,
                 tx: Some(tx.clone()),
                 token: pool.address.clone(),
-                delta: self.value
-                        .neg()
-                        .clone()
-                        .to_signed_bytes_be(),
+                delta: self
+                    .value
+                    .neg()
+                    .clone()
+                    .to_signed_bytes_be(),
                 component_id: pool
                     .address
                     .clone()
@@ -34,15 +36,16 @@ impl BalanceEventTrait for Transfer {
                     .to_vec(),
             })
         }
-        //joining a pool, lp_tokens are minted to the pool, from the null address, and then transferred to the user so thats a positive delta 
+        //joining a pool, lp_tokens are minted to the pool, from the null address, and then transferred to the user so thats a positive delta
         //https://etherscan.io/tx/0x8cf1aa1902994eeaa59b886c848af57d89fec7170c66ef68a541fbc5759e5077#eventlog#387
-        else if event.address == pool.address && event.topics.get(2).unwrap()[12..] == NULL_ADDRESS {
+        else if event.address == pool.address
+            && event.topics.get(2).unwrap()[12..] == NULL_ADDRESS
+        {
             changed_balances.push(BalanceDelta {
                 ord: event.ordinal,
                 tx: Some(tx.clone()),
                 token: pool.address.clone(),
-                delta: self.value
-                        .to_signed_bytes_be(),
+                delta: self.value.to_signed_bytes_be(),
                 component_id: pool
                     .address
                     .clone()
@@ -50,7 +53,7 @@ impl BalanceEventTrait for Transfer {
                     .as_bytes()
                     .to_vec(),
             })
-        } 
+        }
         changed_balances
     }
 }
--- substreams/ethereum-cowamm/src/modules/1_map_cowpool_creations.rs
+++ substreams/ethereum-cowamm/src/modules/1_map_cowpool_creations.rs
@@ -1,37 +1,34 @@
-use crate::{modules::utils::Params};
+use crate::modules::utils::Params;
 use crate::pb::cowamm::{CowPoolCreation, CowPoolCreations};
 use anyhow::{Ok, Result};
-use substreams_ethereum::pb::eth::v2::{Block};
+use substreams_ethereum::pb::eth::v2::Block;
 use substreams_helper::hex::Hexable;
-
 
 #[substreams::handlers::map]
 pub fn map_cowpool_creations(params: String, block: Block) -> Result<CowPoolCreations> {
-    const COWAMM_POOL_CREATED_TOPIC: &str = "0x0d03834d0d86c7f57e877af40e26f176dc31bd637535d4ba153d1ac9de88a7ea";
+    const COWAMM_POOL_CREATED_TOPIC: &str =
+        "0x0d03834d0d86c7f57e877af40e26f176dc31bd637535d4ba153d1ac9de88a7ea";
 
     let params = Params::parse_from_query(&params)?;
-    let factory_address = params.decode_addresses().expect("unable to extract factory address");
+    let factory_address = params
+        .decode_addresses()
+        .expect("unable to extract factory address");
 
     let cow_pool_creations = block
         .logs()
-        .filter(|log| {     
-            log.address() == factory_address && 
-            log.topics()
-                .get(0)
-                .map(|t| t.to_hex()) == Some(COWAMM_POOL_CREATED_TOPIC.to_string())
+        .filter(|log| {
+            log.address() == factory_address
+                && log.topics().get(0).map(|t| t.to_hex())
+                    == Some(COWAMM_POOL_CREATED_TOPIC.to_string())
         })
-        .filter_map(|log| {                   
+        .filter_map(|log| {
             let address = &log
                 .topics()
                 .get(1)
                 .map(|topic| topic.as_slice()[12..].to_vec())?; // we get the last 20 bytes which is BCowPool address
-            // filter again , get the token supply of the pool, if it is zero, ignore it and done index it
-            // but now the problem is that from the data it actually shows it has some supply, because its getting its supply from the start of the block, when it was initialized, thats why their supplies are the same 
-            // so we don't bother about it imo, its value will be taken care of in the balances changes 
-            //what we want to do now is to check if lp token balance tracking through the balance is right - check with the transaction hash
             let tx_hash = log.receipt.transaction.hash.clone();
             Some(CowPoolCreation {
-                address: address.clone(), 
+                address: address.clone(),
                 lp_token: address.clone(), //address of lptoken is same as the pool address
                 created_tx_hash: tx_hash,
             })
@@ -39,5 +36,3 @@ pub fn map_cowpool_creations(params: String, block: Block) -> Result<CowPoolCrea
         .collect::<Vec<CowPoolCreation>>();
     Ok(CowPoolCreations { pools: cow_pool_creations })
 }
-
-
