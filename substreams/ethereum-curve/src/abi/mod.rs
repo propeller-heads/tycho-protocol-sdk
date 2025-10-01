7fadb0459c309cdd16fabdc9292a26b6df143256
zach <zachchou016@gmail.com>
2025-10-01 15:59:20 +0800

    feat: support set_oracle and specific impl dependency

--- substreams/ethereum-curve/src/abi/mod.rs
+++ substreams/ethereum-curve/src/abi/mod.rs
@@ -4,6 +4,7 @@ pub mod crypto_swap_ng_factory;
 pub mod erc20;
 pub mod meta_pool_factory;
 pub mod meta_registry;
+pub mod set_oracle_implementation;
 pub mod stableswap_factory;
 pub mod tricrypto_factory;
 pub mod twocrypto_factory;
