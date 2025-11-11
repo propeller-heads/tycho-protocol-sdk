2211face0676ac50293b7606654d7f486568b4d9
kayibal <alan@propellerheads.xyz>
2025-11-11 17:41:44 +0000

    feat: Implement FluidV1 natively

--- protocol-testing/src/state_registry.rs
+++ protocol-testing/src/state_registry.rs
@@ -2,7 +2,8 @@ use tycho_simulation::{
     evm::{
         engine_db::tycho_db::PreCachedDB,
         protocol::{
-            ekubo::state::EkuboState, pancakeswap_v2::state::PancakeswapV2State,
+            ekubo::state::EkuboState, fluid::FluidV1,
+            pancakeswap_v2::state::PancakeswapV2State,
             rocketpool::state::RocketpoolState, uniswap_v2::state::UniswapV2State,
             uniswap_v3::state::UniswapV3State, uniswap_v4::state::UniswapV4State,
             vm::state::EVMPoolState,
@@ -56,6 +57,12 @@ pub fn register_protocol(
                 decoder_context,
             ),
         "rocketpool" => stream_builder.exchange_with_decoder_context::<RocketpoolState>(
+            protocol_system,
+            tvl_filter,
+            None,
+            decoder_context,
+        ),
+        "fluid_v1" => stream_builder.exchange_with_decoder_context::<FluidV1>(
             protocol_system,
             tvl_filter,
             None,
