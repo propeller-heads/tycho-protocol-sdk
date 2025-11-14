0a1ccc1b1bd837614855504277cc2430327b3cfc
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-11-14 13:22:28 +0100

    feat: extract each step into its own module

---
+++ substreams/ethereum-lido/src/modules/1_map_components.rs
@@ -0,0 +1,66 @@
+//! Template for Protocols with contract factories
+//!
+//! This template provides foundational maps and store substream modules for indexing a
+//! protocol where each component (e.g., pool) is deployed to a separate contract. Each
+//! contract is expected to escrow its ERC-20 token balances.
+//!
+//! If your protocol supports native ETH, you may need to adjust the balance tracking
+//! logic in `map_relative_component_balance` to account for native token handling.
+//!
+//! ## Assumptions
+//! - Assumes each pool has a single newly deployed contract linked to it
+//! - Assumes pool identifier equals the deployed contract address
+//! - Assumes any price or liquidity updated correlates with a pools contract storage update.
+//!
+//! ## Alternative Module
+//! If your protocol uses a vault-like contract to manage balances, or if pools are
+//! registered within a singleton contract, refer to the `ethereum-template-singleton`
+//! substream for an appropriate alternative.
+//!
+//! ## Warning
+//! This template provides a general framework for indexing a protocol. However, it is
+//! likely that you will need to adapt the steps to suit your specific use case. Use the
+//! provided code with care and ensure you fully understand each step before proceeding
+//! with your implementation.
+//!
+//! ## Example Use Case
+//! For an Uniswap-like protocol where each liquidity pool is deployed as a separate
+//! contract, you can use this template to:
+//! - Track relative component balances (e.g., ERC-20 token balances in each pool).
+//! - Index individual pool contracts as they are created by the factory contract.
+//!
+//! Adjustments to the template may include:
+//! - Handling native ETH balances alongside token balances.
+//! - Customizing indexing logic for specific factory contract behavior.
+use crate::pool_factories::{self};
+use anyhow::Result;
+use substreams_ethereum::pb::eth;
+use tycho_substreams::prelude::*;
+
+/// Find and create all relevant protocol components
+///
+/// This method maps over blocks and instantiates ProtocolComponents with a unique ids
+/// as well as all necessary metadata for routing and encoding.
+#[substreams::handlers::map]
+fn map_protocol_components(block: eth::v2::Block) -> Result<BlockTransactionProtocolComponents> {
+    // TODO: add flag to emit only once
+    Ok(BlockTransactionProtocolComponents {
+        tx_components: block
+            .transactions()
+            .filter_map(|tx| {
+                let components = tx
+                    .logs_with_calls()
+                    .filter_map(|(log, call)| {
+                        pool_factories::maybe_create_component(call.call, log, tx)
+                    })
+                    .collect::<Vec<_>>();
+
+                if !components.is_empty() {
+                    Some(TransactionProtocolComponents { tx: Some(tx.into()), components })
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>(),
+    })
+}
