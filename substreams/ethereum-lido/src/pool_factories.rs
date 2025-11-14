efb3befae1dc59ff3f507834d700d4fb024300b8
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-11-14 12:40:27 +0100

    feat: create protocol components

--- substreams/ethereum-lido/src/pool_factories.rs
+++ substreams/ethereum-lido/src/pool_factories.rs
@@ -1,44 +1,98 @@
-use substreams::hex;
+use substreams::{hex, scalar::BigInt};
 use substreams_ethereum::pb::eth::v2::{Call, Log, TransactionTrace};
 use tycho_substreams::models::{
-    ChangeType, FinancialType, ImplementationType, ProtocolComponent, ProtocolType,
+    Attribute, ChangeType, FinancialType, ImplementationType, ProtocolComponent, ProtocolType,
 };
+
+#[derive(Clone, Copy, Debug)]
+pub enum StakingStatus {
+    Limited = 0,
+    Paused = 1,
+    Unlimited = 2,
+}
+
+impl StakingStatus {
+    pub fn as_str_name(&self) -> &'static str {
+        match self {
+            StakingStatus::Limited => "Limited",
+            StakingStatus::Paused => "Paused",
+            StakingStatus::Unlimited => "Unlimited",
+        }
+    }
+}
 
 /// Potentially constructs a new ProtocolComponent given a call
 ///
 /// This method is given each individual call within a transaction, the corresponding
 /// logs emitted during that call as well as the full transaction trace.
-///
-/// If this call creates a component in your protocol please contstruct and return it
-/// here. Otherwise, simply return None.
 pub fn maybe_create_component(
     call: &Call,
     _log: &Log,
     _tx: &TransactionTrace,
 ) -> Option<ProtocolComponent> {
     match *call.address {
-        // TODO: replace with your logic
-        hex!("0000000000000000000000000000000000000000") => {
-            Some(ProtocolComponent {
-                id: "".to_string(),
-                tokens: vec![
-                    // TODO: add the components tokens
-                ],
-                contracts: vec![
-                    // TODO: any contracts required during swapping
-                ],
-                static_att: vec![
-                    // TODO: any additional metadata required, e.g. for swap encoding
-                ],
+        hex!("17144556fd3424EDC8Fc8A4C940B2D04936d17eb") => Some(ProtocolComponent {
+            id: "stETH".to_string(),
+            tokens: vec![
+                hex!("ae7ab96520DE3A18E5e111B5EaAb095312D7fE84").into(),
+                hex!("EeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").into(),
+            ],
+            contracts: vec![
+                hex!("ae7ab96520DE3A18E5e111B5EaAb095312D7fE84").into(),
+                hex!("17144556fd3424EDC8Fc8A4C940B2D04936d17eb").into(),
+            ],
+            static_att: vec![
+                Attribute {
+                    name: "total_shares".to_string(),
+                    value: BigInt::from(0).to_signed_bytes_be(),
+                    change: ChangeType::Creation.into(),
+                },
+                Attribute {
+                    name: "total_pooled_eth".to_string(),
+                    value: BigInt::from(0).to_signed_bytes_be(),
+                    change: ChangeType::Creation.into(),
+                },
+                Attribute {
+                    name: "staking_status".to_string(),
+                    value: StakingStatus::Limited
+                        .as_str_name()
+                        .into(),
+                    change: ChangeType::Creation.into(),
+                },
+                Attribute {
+                    name: "staking_limit".to_string(),
+                    value: BigInt::from(0).to_signed_bytes_be(),
+                    change: ChangeType::Creation.into(),
+                },
+            ],
+            change: ChangeType::Creation.into(),
+            protocol_type: Some(ProtocolType {
+                name: "stETH".to_string(),
+                financial_type: FinancialType::Swap.into(),
+                attribute_schema: vec![],
+                implementation_type: ImplementationType::Vm.into(),
+            }),
+        }),
+        hex!("7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0") => Some(ProtocolComponent {
+            id: "wstETH".to_string(),
+            tokens: vec![
+                hex!("ae7ab96520DE3A18E5e111B5EaAb095312D7fE84").into(),
+                hex!("7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0").into(),
+            ],
+            contracts: vec![hex!("7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0").into()],
+            static_att: vec![Attribute {
+                name: "total_wstETH".to_string(),
+                value: BigInt::from(0).to_signed_bytes_be(),
                 change: ChangeType::Creation.into(),
-                protocol_type: Some(ProtocolType {
-                    name: "template".to_string(),
-                    financial_type: FinancialType::Swap.into(),
-                    attribute_schema: vec![],
-                    implementation_type: ImplementationType::Vm.into(),
-                }),
-            })
-        }
+            }],
+            change: ChangeType::Creation.into(),
+            protocol_type: Some(ProtocolType {
+                name: "wstETH".to_string(),
+                financial_type: FinancialType::Swap.into(),
+                attribute_schema: vec![],
+                implementation_type: ImplementationType::Vm.into(),
+            }),
+        }),
         _ => None,
     }
 }
