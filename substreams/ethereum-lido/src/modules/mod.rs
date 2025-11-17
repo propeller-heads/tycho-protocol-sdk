a8cba87cfb874884cb996a702f3d89f9080edc5e
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-11-17 15:28:28 +0100

    chore: remove unused modules

--- substreams/ethereum-lido/src/modules/mod.rs
+++ substreams/ethereum-lido/src/modules/mod.rs
@@ -1,7 +1,5 @@
 pub use map_component_balance::map_component_balance;
-pub use map_protocol_changes::map_protocol_changes;
 pub use map_protocol_components::map_protocol_components;
-pub use store_balances::store_balances;
 pub use store_protocol_components::store_protocol_components;
 
 #[path = "1_map_components.rs"]
@@ -12,9 +10,3 @@ mod store_protocol_components;
 
 #[path = "3_map_component_balance.rs"]
 mod map_component_balance;
-
-#[path = "4_store_balances.rs"]
-mod store_balances;
-
-#[path = "5_map_protocol_changes.rs"]
-mod map_protocol_changes;
