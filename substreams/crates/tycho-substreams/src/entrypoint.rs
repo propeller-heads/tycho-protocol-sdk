2af1e9fac2d1917682d1ddbcec43e7540b07028a
Louise Poole <louise@datarevenue.com>
2025-11-17 13:12:46 +0200

    feat: update entrypoint creation requirements
    Make component link to created entrypoint/params required.

--- substreams/crates/tycho-substreams/src/entrypoint.rs
+++ substreams/crates/tycho-substreams/src/entrypoint.rs
@@ -6,6 +6,18 @@ fn get_entrypoint_id(target: &[u8], signature: &str) -> String {
 }
 
 /// Creates an entrypoint and its parameters.
+///
+/// These are used by the Dynamic Contract Indexer (DCI) to dynamically trace contract calls and
+/// index relevant code and storage.
+///
+/// # Parameters
+/// - `target`: The target contract to analyse this entrypoint on.
+/// - `signature`: The signature of the function to analyse.
+/// - `component_id`: The id of the component that uses this entrypoint.
+/// - `trace_data`: The trace data to be used to trace the entrypoint.
+///
+/// # Returns
+/// - `(EntryPoint, EntryPointParams)`: A tuple containing the entrypoint and parameter messages.
 pub fn create_entrypoint(
     target: Vec<u8>,
     signature: String,
@@ -25,28 +37,4 @@ pub fn create_entrypoint(
         trace_data: Some(trace_data),
     };
     (entrypoint, entrypoint_params)
-}
-
-// Adds EntryPointParams associated with an already existing Entrypoint.
-pub fn add_entrypoint_params(
-    target: Vec<u8>,
-    signature: String,
-    trace_data: TraceData,
-    component_id: Option<String>,
-) -> EntryPointParams {
-    EntryPointParams {
-        entrypoint_id: get_entrypoint_id(&target, &signature),
-        component_id,
-        trace_data: Some(trace_data),
-    }
-}
-
-// Adds a component to an existing Entrypoint.
-pub fn add_component_to_entrypoint(
-    target: Vec<u8>,
-    signature: String,
-    component_id: String,
-) -> EntryPoint {
-    let entrypoint_id = get_entrypoint_id(&target, &signature);
-    EntryPoint { id: entrypoint_id, target, signature, component_id }
 }
