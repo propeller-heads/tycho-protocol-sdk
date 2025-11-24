7550e4c79ae2c669e039e2097d61a59b19ffd122
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-11-24 10:49:51 +0100

    refactor: rename remaining components after removing unneeded one

--- substreams/ethereum-lido/src/modules/mod.rs
+++ substreams/ethereum-lido/src/modules/mod.rs
@@ -5,7 +5,7 @@ use substreams::hex;
 #[path = "1_map_components.rs"]
 mod map_protocol_components;
 
-#[path = "3_map_component_balance.rs"]
+#[path = "2_map_component_balance.rs"]
 mod map_component_balance;
 
 pub const ST_ETH_ADDRESS_OUTER: [u8; 20] = hex!("ae7ab96520de3a18e5e111b5eaab095312d7fe84");
