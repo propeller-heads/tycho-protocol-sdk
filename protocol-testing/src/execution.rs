fca7b05e1ace46a166cd902c474653e47967129e
TAMARA LIPOWSKI <data.lipowski@extaccount.com>
2025-11-28 12:24:50 -0500

    docs: comment about replacing hook bytecode with your own

--- protocol-testing/src/execution.rs
+++ protocol-testing/src/execution.rs
@@ -38,6 +38,8 @@ static EXECUTOR_MAPPING: LazyLock<HashMap<&'static str, &'static str>> = LazyLoc
     map.insert("uniswap_v3", UNISWAP_V3_BYTECODE_JSON);
     map.insert("pancakeswap_v3", UNISWAP_V3_BYTECODE_JSON);
     map.insert("uniswap_v4", UNISWAP_V4_BYTECODE_JSON);
+    // If you would like to test any other hook, replace this bytecode with the
+    // desired hook bytecode
     map.insert("uniswap_v4_hooks", UNISWAP_V4_ANGSTROM_BYTECODE_JSON);
     map.insert("vm:balancer_v2", BALANCER_V2_BYTECODE_JSON);
     map.insert("vm:balancer_v3", BALANCER_V3_BYTECODE_JSON);
