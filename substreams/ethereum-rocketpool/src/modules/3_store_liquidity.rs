f7918199afc469adab7f9c3e5cbc537c462c90f5
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-11-28 11:38:24 +0000

    fix: have liquidity as a attribute, and total eth as a component balance

--- substreams/ethereum-rocketpool/src/modules/3_store_liquidity.rs
+++ substreams/ethereum-rocketpool/src/modules/3_store_liquidity.rs
@@ -3,6 +3,6 @@ use tycho_substreams::models::BlockBalanceDeltas;
 
 /// Aggregates relative eth balance values into absolute values
 #[substreams::handlers::store]
-pub fn store_balance(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
+pub fn store_liquidity(deltas: BlockBalanceDeltas, store: StoreAddBigInt) {
     tycho_substreams::balances::store_balance_changes(deltas, store);
 }
