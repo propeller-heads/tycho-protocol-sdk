35de255693a36d67b44fe1d3d878c5d5b34c690b
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-12-03 14:58:18 +0000

    fix: support both rocket_network_balances v2 and v3, update references in protocol mapping

--- substreams/ethereum-rocketpool/src/abi/mod.rs
+++ substreams/ethereum-rocketpool/src/abi/mod.rs
@@ -3,5 +3,6 @@
 pub mod rocket_dao_protocol_proposal;
 pub mod rocket_deposit_pool;
 pub mod rocket_minipool_queue;
-pub mod rocket_network_balances;
+pub mod rocket_network_balances_v2;
+pub mod rocket_network_balances_v3;
 pub mod rocket_token_reth;
