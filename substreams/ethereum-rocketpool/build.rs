d096578704081c47d868bf20875f04465e84d891
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-12-03 10:28:49 +0000

    chore: making fmt happy

--- substreams/ethereum-rocketpool/build.rs
+++ substreams/ethereum-rocketpool/build.rs
@@ -6,7 +6,11 @@ use substreams_ethereum::Abigen;
 fn main() -> Result<()> {
     for entry in fs::read_dir("abi")? {
         let path = entry?.path();
-        let name = path.file_stem().unwrap().to_str().unwrap();
+        let name = path
+            .file_stem()
+            .unwrap()
+            .to_str()
+            .unwrap();
         Abigen::new(name, path.to_str().unwrap())?
             .generate()?
             .write_to_file(format!("src/abi/{}.rs", name.to_snake_case()))?;
