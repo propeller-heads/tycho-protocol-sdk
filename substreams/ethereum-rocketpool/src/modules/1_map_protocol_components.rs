b3b5b0317871a0f7339b1b6076cc7edefbdf8d54
Artem <48178500+ElusAegis@users.noreply.github.com>
2025-12-04 15:39:33 +0000

    fix: update mapping logic to find the transaction that activated the Rocket Pool Deposit Pool V1.2

--- substreams/ethereum-rocketpool/src/modules/1_map_protocol_components.rs
+++ substreams/ethereum-rocketpool/src/modules/1_map_protocol_components.rs
@@ -1,4 +1,6 @@
-use crate::constants::{ETH_ADDRESS, RETH_ADDRESS, ROCKET_POOL_COMPONENT_ID};
+use crate::constants::{
+    ETH_ADDRESS, RETH_ADDRESS, ROCKET_POOL_COMPONENT_ID, ROCKET_STORAGE_ADDRESS,
+};
 use anyhow::Result;
 use substreams_ethereum::pb::eth;
 use tycho_substreams::models::{
@@ -6,10 +8,13 @@ use tycho_substreams::models::{
     TransactionProtocolComponents,
 };
 
-/// Find and create all relevant protocol components
+/// Find and create all relevant protocol components.
 ///
-/// This method maps over blocks and instantiates ProtocolComponents with a unique ids
-/// as well as all necessary metadata for routing and encoding.
+/// As Rocket Pool has a single deposit pool that supports exchanging between ETH and rETH, we
+/// emit a single hardcoded ProtocolComponent at the specified block based on the state of
+/// the Rocket Pool protocol at that time, which we pass in via the `params`.
+///
+/// We return early for all other blocks since ProtocolComponents only need to be emitted once.
 #[substreams::handlers::map]
 fn map_protocol_components(
     params: String,
@@ -19,10 +24,14 @@ fn map_protocol_components(
         return Ok(BlockTransactionProtocolComponents { tx_components: vec![] });
     }
 
-    // We assume that the first transaction in the block is the Rocket Deposit Pool creation
+    // Find the transaction that activated the deposit pool V1.2 address in the Rocket Storage
     let tx = block
         .transactions()
-        .next()
+        .find(|tx| {
+            tx.calls
+                .iter()
+                .any(|call| call.address == ROCKET_STORAGE_ADDRESS)
+        })
         .ok_or(anyhow::anyhow!("No transaction found for Rocket Deposit Pool"))?;
 
     let component = ProtocolComponent::new(ROCKET_POOL_COMPONENT_ID)
