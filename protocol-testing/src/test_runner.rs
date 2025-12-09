f26cbbe307d8ae731ad592557e0331ce73e1312a
Ksenia Troshchenkova <ksenia@propellerheads.xyz>
2025-12-09 18:07:31 +0100

    chore: revert permutation logic changes; move to a separate PR

--- protocol-testing/src/test_runner.rs
+++ protocol-testing/src/test_runner.rs
@@ -1053,9 +1053,6 @@ impl TestRunner {
             let swap_directions: Vec<_> = tokens
                 .iter()
                 .permutations(2)
-                .filter(|perm| {
-                    perm[1].address != Bytes::from("0x0000000000000000000000000000000000000000")
-                }) // remove if second element is zero
                 .map(|perm| (perm[0], perm[1]))
                 .collect();
 
