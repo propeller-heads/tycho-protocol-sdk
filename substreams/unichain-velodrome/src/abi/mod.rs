25e175bd7d92362f6c413faa000a069fea8850e7
zach <zachchou016@gmail.com>
2025-12-13 22:57:39 +0800

    feat: init velodrome

---
+++ substreams/unichain-velodrome/src/abi/mod.rs
@@ -0,0 +1,5 @@
+#![allow(clippy::all, clippy::pedantic, clippy::nursery)]
+
+pub mod dynamic_swap_fee_module;
+pub mod factory;
+pub mod pool;
---
+++ substreams/unichain-velodrome/src/pb/mod.rs
@@ -0,0 +1,10 @@
+// @generated
+pub mod tycho {
+    pub mod evm {
+        // @@protoc_insertion_point(attribute:tycho.evm.aerodrome)
+        pub mod aerodrome {
+            include!("tycho.evm.aerodrome.rs");
+            // @@protoc_insertion_point(tycho.evm.aerodrome)
+        }
+    }
+}
