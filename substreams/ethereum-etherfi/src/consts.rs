406f2554c76bfe8094795200854c10002803036b
zach <zachchou016@gmail.com>
2026-01-27 19:38:21 +0800

    chore: remove unused commented constants

--- substreams/ethereum-etherfi/src/consts.rs
+++ substreams/ethereum-etherfi/src/consts.rs
@@ -1,12 +1,8 @@
 use substreams::hex;
 
-// pub const ZERO_ADDRESS: &[u8] = &[0u8; 20];
 pub const ETH_ADDRESS: [u8; 20] = hex!("EeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE");
 
 pub const EETH_ADDRESS: [u8; 20] = hex!("35fA164735182de50811E8e2E824cFb9B6118ac2");
-// pub const EETH_CREATION_BLOCK: u64 = 17664324;
-// pub const EETH_CREATION_TX: &str =
-//     "0xf6763c707b90b260bba114fce9a141aa4a923327539ded9d4d4ae4395b2200ff";
 
 pub const LIQUIDITY_POOL_ADDRESS: [u8; 20] = hex!("308861a430be4cce5502d0a12724771fc6daf216");
 pub const LIQUIDITY_POOL_CREATION_BLOCK: u64 = 17664317;
@@ -19,6 +15,3 @@ pub const WEETH_CREATION_TX: [u8; 32] =
     hex!("a034bdf7ec3b407125fcfbb786d908b0bcfd9976f2fbaf489776ba58b9db61ac");
 
 pub const REDEMPTION_MANAGER_ADDRESS: [u8; 20] = hex!("DadEf1fFBFeaAB4f68A9fD181395F68b4e4E7Ae0");
-// pub const REDEMPTION_MANAGER_CREATION_BLOCK: u64 = 22090400;
-// pub const REDEMPTION_MANAGER_CREATION_TX: [u8; 32] =
-//     hex!("d9f97b2ed706d46f0fd79f9eb173fe6373488fbdb1c80299f30e65459961ae9f");
